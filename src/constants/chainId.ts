ce490f7741c1d4b1abbfbebbc021f38470962c76
Michał Ziętalak <michal.z@ethworks.io>
2020-12-18 15:42:19 +0100

    😾 Remove unused currencies

--- src/constants/chainId.ts
+++ src/constants/chainId.ts
@@ -1,9 +1,6 @@
 export enum ChainId {
   Mainnet = 1,
-  Ropsten = 3,
-  Rinkeby = 4,
-  Goerli = 5,
   Kovan = 42,
 }
 
-export const SUPPORTED_CHAINS = [ChainId.Mainnet, ChainId.Ropsten, ChainId.Rinkeby, ChainId.Goerli, ChainId.Kovan]
+export const SUPPORTED_CHAINS = [ChainId.Mainnet, ChainId.Kovan]
