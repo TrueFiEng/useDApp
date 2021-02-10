1ef74197d798639ad10cfc2d7ab92fe86cc736a1
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-02-10 15:14:17 +0100

    ❄️ Extract playground into a separate package (#22)
    * Extract playground into a separate package
    
    * Remove submodule
    
    * Commit example code
    
    * Clean up
    
    * Install
    
    * Remove web-test-runner
    
    * Lint
    
    * Switch to commonjs
    
    * More chains
    
    * Update packages/example/package.json
    
    * Lint
    
    Co-authored-by: rzadp <roopert7@gmail.com>
    Co-authored-by: Przemyslaw Rzad <przemek@ethworks.io>

---
+++ packages/example/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useBlockNumber, useBlockMeta, useEthers } from '@usedapp/core'
+import { InjectedConnector } from '@web3-react/injected-connector'
+
+const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 42] })
+
+export function App() {
+  const blockNumber = useBlockNumber()
+  const { chainId, activate } = useEthers()
+  const { timestamp, difficulty } = useBlockMeta()
+
+  return (
+    <div className="App">
+      <p>Chain id: {chainId}</p>
+      <p>Current block: {blockNumber}</p>
+      {difficulty && <p>Current difficulty: {difficulty.toString()}</p>}
+      {timestamp && <p>Current block timestamp: {timestamp.toLocaleString()}</p>}
+      <div>
+        <button onClick={() => activate(injected)}>Connect</button>
+      </div>
+    </div>
+  )
+}
