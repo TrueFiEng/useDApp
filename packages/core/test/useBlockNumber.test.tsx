458df3faabb50b8c916c80fda5aacf662ad2961c
Przemyslaw Rzad <przemek@ethworks.io>
2021-02-11 14:33:42 +0200

    📛 Rename mock classes (#27)
    * 📛 Rename testConnector to MockConnector
    
    * Use address zero
    
    * Rename web3wrapper

--- packages/core/test/useBlockNumber.test.tsx
+++ packages/core/test/useBlockNumber.test.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { expect } from 'chai'
 import { renderHook } from '@testing-library/react-hooks'
 import { useBlockNumber, BlockNumberProvider } from '../src'
-import { TestConnector, Web3Wrapper, waitUntil } from './utils'
+import { MockConnector, MockWeb3Wrapper, waitUntil } from './utils'
 import { MockProvider } from '@ethereum-waffle/provider'
+import { AddressZero } from '@ethersproject/constants'
 
 describe('useBlockNumber', () => {
   it('retrieves block number', async () => {
     const {result, waitForValueToChange} = renderHook(useBlockNumber, {
-      wrapper: ({children}) => <Web3Wrapper><BlockNumberProvider>{children}</BlockNumberProvider></Web3Wrapper>
+      wrapper: ({children}) => <MockWeb3Wrapper><BlockNumberProvider>{children}</BlockNumberProvider></MockWeb3Wrapper>
     })
 
     await waitForValueToChange(() => result.current)
@@ -18,17 +19,17 @@ describe('useBlockNumber', () => {
 
   it('updates the block number when a transaction gets mined', async () => {
     const provider = new MockProvider()
-    const connector = new TestConnector(provider)
+    const connector = new MockConnector(provider)
 
     const {result, waitForValueToChange} = renderHook(useBlockNumber, {
-      wrapper: ({children}) => <Web3Wrapper connector={connector}><BlockNumberProvider>{children}</BlockNumberProvider></Web3Wrapper>
+      wrapper: ({children}) => <MockWeb3Wrapper connector={connector}><BlockNumberProvider>{children}</BlockNumberProvider></MockWeb3Wrapper>
     })
 
     await waitUntil(() => result.current === 0)
     
     // send a mock transaction
     const [acc] = await provider.getWallets()
-    const tx = await acc.sendTransaction({to: '0x0000000000000000000000000000000000000000', value: 1})
+    const tx = await acc.sendTransaction({to: AddressZero, value: 1})
     await tx.wait()
 
     await waitForValueToChange(() => result.current, {timeout: 4000})
--- packages/core/test/utils/mockWeb3Wrapper.tsx
+++ packages/core/test/utils/mockWeb3Wrapper.tsx
@@ -2,18 +2,18 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 import { Web3ReactProvider } from '@web3-react/core'
 import React, { ReactNode, useEffect} from 'react'
 import { useEthers } from '../../src'
-import { TestConnector } from './testConnector'
+import { MockConnector } from './mockConnector'
 
-export interface Web3WrapperProps {
+export interface MockWeb3WrapperProps {
   connector?: AbstractConnector,
   children?: ReactNode;
 }
 
-const WrapperActivation = ({children, connector}: Web3WrapperProps) => {
+const WrapperActivation = ({children, connector}: MockWeb3WrapperProps) => {
   const {activate, active} = useEthers()
 
   useEffect(() => {
-    activate(connector ?? new TestConnector(), console.error)
+    activate(connector ?? new MockConnector(), console.error)
   }, [])
 
   if (!active) return null
@@ -22,7 +22,7 @@ const WrapperActivation = ({children, connector}: Web3WrapperProps) => {
 
 
 
-export const Web3Wrapper = ({children, connector}: Web3WrapperProps) => {
+export const MockWeb3Wrapper = ({children, connector}: MockWeb3WrapperProps) => {
   return (
     <Web3ReactProvider getLibrary={(provider) => provider}>
       <WrapperActivation connector={connector}>
