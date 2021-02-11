d97688607fa19893a36f6e0668c1c7d670688e6b
rzadp <roopert7@gmail.com>
2021-02-11 17:09:11 +0200

    Lint

--- packages/core/src/testing/mockWeb3Wrapper.tsx
+++ packages/core/src/testing/mockWeb3Wrapper.tsx
@@ -1,16 +1,16 @@
 import { AbstractConnector } from '@web3-react/abstract-connector'
 import { Web3ReactProvider } from '@web3-react/core'
-import React, { ReactNode, useEffect} from 'react'
+import React, { ReactNode, useEffect } from 'react'
 import { useEthers } from '..'
 import { MockConnector } from './mockConnector'
 
 export interface MockWeb3WrapperProps {
-  connector?: AbstractConnector,
-  children?: ReactNode;
+  connector?: AbstractConnector
+  children?: ReactNode
 }
 
-const WrapperActivation = ({children, connector}: MockWeb3WrapperProps) => {
-  const {activate, active} = useEthers()
+const WrapperActivation = ({ children, connector }: MockWeb3WrapperProps) => {
+  const { activate, active } = useEthers()
 
   useEffect(() => {
     activate(connector ?? new MockConnector(), console.error)
@@ -20,14 +20,10 @@ const WrapperActivation = ({children, connector}: MockWeb3WrapperProps) => {
   return <>{children}</>
 }
 
-
-
-export const MockWeb3Wrapper = ({children, connector}: MockWeb3WrapperProps) => {
+export const MockWeb3Wrapper = ({ children, connector }: MockWeb3WrapperProps) => {
   return (
     <Web3ReactProvider getLibrary={(provider) => provider}>
-      <WrapperActivation connector={connector}>
-        {children}
-      </WrapperActivation>
+      <WrapperActivation connector={connector}>{children}</WrapperActivation>
     </Web3ReactProvider>
   )
 }
--- packages/core/src/testing/renderWeb3Hook.tsx
+++ packages/core/src/testing/renderWeb3Hook.tsx
@@ -1,5 +1,5 @@
 import { MockProvider } from '@ethereum-waffle/provider'
-import {deployContract} from 'ethereum-waffle'
+import { deployContract } from 'ethereum-waffle'
 import { renderHook, RenderHookOptions } from '@testing-library/react-hooks'
 import { ReactNode } from 'react'
 import { BlockNumberProvider, ChainStateProvider } from '../providers'
@@ -10,38 +10,39 @@ import { waitUntil } from './utils'
 import { MULTICALL_BYTECODE } from '../constants'
 import MultiCall from '../constants/MultiCall.json'
 
-const IdentityWrapper = ({children}: {children: ReactNode}) => <>{children}</>
+const IdentityWrapper = ({ children }: { children: ReactNode }) => <>{children}</>
 
 const mineBlock = async (provider: MockProvider) => {
   const [acc] = await provider.getWallets()
-  const tx = await acc.sendTransaction({to: AddressZero, value: 0})
+  const tx = await acc.sendTransaction({ to: AddressZero, value: 0 })
   await tx.wait()
 }
 
-export const renderWeb3Hook = async (hook: (props: unknown) => unknown, options?: RenderHookOptions<unknown> | undefined) => {
+export const renderWeb3Hook = async (
+  hook: (props: unknown) => unknown,
+  options?: RenderHookOptions<unknown> | undefined
+) => {
   const provider = new MockProvider()
   const connector = new MockConnector(provider)
   const UserWrapper = options?.wrapper ?? IdentityWrapper
 
-  const multicall = await deployContract(
-    (await provider.getWallets())[0],
-    {bytecode: MULTICALL_BYTECODE, abi: MultiCall}
-  )
-  const multicallAddresses = {[await connector.getChainId()]: multicall.address}
+  const multicall = await deployContract((await provider.getWallets())[0], {
+    bytecode: MULTICALL_BYTECODE,
+    abi: MultiCall,
+  })
+  const multicallAddresses = { [await connector.getChainId()]: multicall.address }
 
-  const {result, waitForNextUpdate, rerender, unmount} = renderHook(hook, {
+  const { result, waitForNextUpdate, rerender, unmount } = renderHook(hook, {
     ...options,
-    wrapper: ({children}) => (
+    wrapper: ({ children }) => (
       <MockWeb3Wrapper connector={connector}>
         <BlockNumberProvider>
           <ChainStateProvider multicallAddresses={multicallAddresses}>
-            <UserWrapper>
-              {children}
-            </UserWrapper>
+            <UserWrapper>{children}</UserWrapper>
           </ChainStateProvider>
         </BlockNumberProvider>
       </MockWeb3Wrapper>
-    )
+    ),
   })
 
   // we wait for the first update, before that the current is always undefined.
