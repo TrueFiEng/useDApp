0aa4a5e226d0e9cade34b027bd85431e151e713d
Dmytro Maretskyi <maretskii@gmail.com>
2021-02-16 15:26:49 +0100

    Extract call data

--- packages/core/src/hooks/useBlockMeta.ts
+++ packages/core/src/hooks/useBlockMeta.ts
@@ -3,10 +3,13 @@ import { BigNumber } from '@ethersproject/bignumber'
 import { useChainCall } from './useChainCalls'
 import { useMulticallAddress } from './useMulticallAddress'
 
+const GET_CURRENT_BLOCK_TIMESTAMP_CALL = MultiCallABI.encodeFunctionData('getCurrentBlockTimestamp', [])
+const GET_CURRENT_BLOCK_DIFFICULTY_CALL = MultiCallABI.encodeFunctionData('getCurrentBlockDifficulty', [])
+
 export function useBlockMeta() {
   const address = useMulticallAddress()
-  const timestamp = useChainCall({ address, data: MultiCallABI.encodeFunctionData('getCurrentBlockTimestamp', []) })
-  const difficulty = useChainCall({ address, data: MultiCallABI.encodeFunctionData('getCurrentBlockDifficulty', []) })
+  const timestamp = useChainCall({ address, data: GET_CURRENT_BLOCK_TIMESTAMP_CALL })
+  const difficulty = useChainCall({ address, data: GET_CURRENT_BLOCK_DIFFICULTY_CALL })
 
   return {
     timestamp: timestamp !== undefined ? new Date(BigNumber.from(timestamp).mul(1000).toNumber()) : undefined,
