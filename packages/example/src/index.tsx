9b7ec877a4f95f1817769d0467ce21527d514666
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2021-02-21 19:36:55 +0100

    Introduce Config, rename Providers to DAppProvider, extract ReadOnlyProviderActivator to a seperate file

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ChainId, Providers } from '@usedapp/core'
+import { ChainId, DAppProvider } from '@usedapp/core'
 import { App } from './App'
+import type { Config } from '@usedapp/core/dist/src/model/Config'
+
+const config: Config = {
+  readOnlyChain: ChainId.Mainnet,
+  readOnlyUrls: {
+    [ChainId.Mainnet]: 'https://mainnet.infura.io/v3/62687d1a985d4508b2b7a24827551934',
+  },
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Providers readOnlyChain={ChainId.Mainnet}>
+    <DAppProvider config={config}>
       <App />
-    </Providers>
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
