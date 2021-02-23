f3d1621a6e7aca93d9c1144e7986a05957425990
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2021-02-23 17:23:12 +0100

    Fix Goerli name and add missing MULTICALL_ADDRESSES

--- packages/core/src/constants/chainId.ts
+++ packages/core/src/constants/chainId.ts
@@ -2,7 +2,7 @@ export enum ChainId {
   Mainnet = 1,
   Ropsten = 3,
   Rinkeby = 4,
-  Gorli = 5,
+  Goerli = 5,
   Kovan = 42,
   xDai = 100,
 }
@@ -12,7 +12,7 @@ export const SUPPORTED_CHAINS = [
   ChainId.Ropsten,
   ChainId.Kovan,
   ChainId.Rinkeby,
-  ChainId.Gorli,
+  ChainId.Goerli,
   ChainId.Kovan,
   ChainId.xDai,
 ]
@@ -21,7 +21,7 @@ export const MULTICALL_ADDRESSES = {
   [ChainId.Mainnet]: '0xeefba1e63905ef1d7acba5a8513c70307c1ce441',
   [ChainId.Ropsten]: '0x53c43764255c17bd724f74c4ef150724ac50a3ed',
   [ChainId.Rinkeby]: '0x42ad527de7d4e9d9d011ac45b31d8551f8fe9821',
-  // [ChainId.Goerli]: '0x77dca2c955b15e9de4dbbcf1246b4b85b651e50e',
+  [ChainId.Goerli]: '0x77dca2c955b15e9de4dbbcf1246b4b85b651e50e',
   [ChainId.Kovan]: '0x2cc8688c5f75e365aaeeb4ea8d6a480405a48d2a',
-  // [ChainId.XDAI]: '0xb5b692a88bdfc81ca69dcb1d924f59f0413a602a',
+  [ChainId.xDai]: '0xb5b692a88bdfc81ca69dcb1d924f59f0413a602a',
 }
