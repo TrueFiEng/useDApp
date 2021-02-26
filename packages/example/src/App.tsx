37e09294f7d8fe4713bc69ed00d8924b66856e78
Dmytro Maretskyi <maretskii@gmail.com>
2021-02-26 11:49:37 +0100

    Style

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,8 +1,9 @@
-import { useBlockMeta, useBlockNumber, useChainCalls, useEthers } from '@usedapp/core'
+import { Token, useBlockMeta, useBlockNumber, useChainCalls, useEthers } from '@usedapp/core'
 import { Interface } from '@ethersproject/abi'
 import { formatUnits } from '@ethersproject/units'
 import { getAddress } from '@ethersproject/address'
 import React, { useEffect, useState } from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
 
 const ERC20Interface = new Interface(['function balanceOf(address) view returns(uint256)'])
 export function App() {
@@ -24,7 +25,8 @@ export function App() {
   })) : []).map(data => data && ERC20Interface.decodeFunctionResult('balanceOf', data)[0])
 
   return (
-    <div className="App">
+    <Background className="App">
+      <Global/>
       <p>Chain id: {chainId}</p>
       <p>Current block: {blockNumber}</p>
       {difficulty && <p>Current difficulty: {difficulty.toString()}</p>}
@@ -34,14 +36,76 @@ export function App() {
         <button onClick={() => deactivate()}>Disconnect</button>
       </div>
       {account && <p>Account: {account}</p>}
-      <ul>
+      <TokenList>
         {tokenList && tokenList.tokens.map((token: any, idx: number) => (
-          <li>
-            <img src={`https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${getAddress(token.address)}/logo.png`}/>
-            {token.name} - {balances?.[idx] && formatUnits(balances[idx], token.decimals)}
-          </li>
+          <TokenItem>
+            <TokenIconContainer>
+              <TokenIcon src={`https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${getAddress(token.address)}/logo.png`}/>
+            </TokenIconContainer>
+            <TokenName>{token.name}</TokenName>
+            <TokenTicker>{token.symbol}</TokenTicker>
+            <TokenBalance>{balances?.[idx] && formatUnits(balances[idx], token.decimals)}</TokenBalance>
+          </TokenItem>
         ))}
-      </ul>
-    </div>
+      </TokenList>
+    </Background>
   )
 }
+
+const Global = createGlobalStyle`
+  * {
+    font-family: 'Open Sans', sans-serif;
+  }
+`
+
+const Background = styled.div`
+  background-color: #f3f4f9;
+`
+
+const TokenList = styled.ul`
+  background-color: white;
+  border-radius: 16px;
+  margin: auto;
+  margin-top: 64px;
+  padding: 16px 16px 16px 16px;
+  max-width: 640px;
+`
+
+const TokenItem = styled.li`
+  display: grid;
+  grid-template-areas:
+    "icon name balance"
+    "icon ticker balance";
+  grid-template-columns: auto 1fr auto;
+  grid-column-gap: 8px;
+  margin: 8px;
+  margin-top: 16px;
+  align-items: center;
+`
+
+const TokenIconContainer = styled.div`
+  grid-area: icon;
+  width: 64px;
+  height: 64px;
+  border-radius: 50%;
+  background-color: #f2f7fb;
+`
+
+const TokenIcon = styled.img`
+  width: 32px;
+  height: 32px;
+  margin: 16px;
+`
+
+const TokenName = styled.span`
+  grid-area: name;
+`
+
+const TokenTicker = styled.span`
+  grid-area: ticker;
+  color: gray;
+`
+
+const TokenBalance = styled.span`
+  grid-area: balance;
+`
