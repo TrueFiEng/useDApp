a1e515f9bf7b867a6faf6ab02cc222b4b8889b04
HubertNastaly <h.nastaly@gmail.com>
2021-02-26 14:52:54 +0100

    contract call

---
+++ packages/core/src/hooks/useContractCall.ts
@@ -0,0 +1,12 @@
+import { Interface } from "@ethersproject/abi"
+import { Falsy } from "../model/types"
+import { useChainCall } from "./useChainCalls"
+
+export function useContractCall(abi: Interface, address: string | Falsy, functionName: string, args: any[] | Falsy): any | undefined {
+  const callData = args && args.every(arg => arg !== undefined) && abi.encodeFunctionData(functionName, args)
+
+  const result = useChainCall(
+    callData && address && { address, data: callData }
+  )
+  return result !== undefined ? abi.decodeFunctionResult(functionName, result) : undefined 
+}
