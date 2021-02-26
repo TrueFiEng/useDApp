1ea0bc22134be1e9aba0c8b56ec34fb9fa1ac9cc
Dmytro Maretskyi <maretskii@gmail.com>
2021-02-26 16:23:44 +0100

    CE

--- packages/core/src/hooks/useTokenBalance.ts
+++ packages/core/src/hooks/useTokenBalance.ts
@@ -1,11 +1,10 @@
 import { Interface } from '@ethersproject/abi'
 import { BigNumber } from '@ethersproject/bignumber'
+import { ERC20ABI } from '../constants'
 import { Falsy } from '../model/types'
 import { useContractCall } from './useContractCall'
 
-const TokenABI = new Interface(['function balanceOf (address) view returns (uint256)'])
-
 export function useTokenBalance(address: string | Falsy, tokenAddress: string | Falsy): BigNumber | undefined {
-  const [tokenBalance] = useContractCall(TokenABI, tokenAddress, 'balanceOf', address && [address]) ?? []
+  const [tokenBalance] = useContractCall(ERC20ABI, tokenAddress, 'balanceOf', address && [address]) ?? []
   return tokenBalance
 }
