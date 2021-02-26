88dfc4e1dd90477678aa994d237a50478dc5f3a8
Przemyslaw Rzad <przemek@ethworks.io>
2021-02-26 15:09:11 +0100

    Add useContractCall (#60)
    * contract call
    
    * Lint
    
    * Lint
    
    * Added changeset
    
    Co-authored-by: HubertNastaly <h.nastaly@gmail.com>

--- packages/core/src/hooks/useEtherBalance.ts
+++ packages/core/src/hooks/useEtherBalance.ts
@@ -1,15 +1,11 @@
 import { MultiCallABI } from '../constants'
-import { useChainCall } from './useChainCalls'
 import { useMulticallAddress } from './useMulticallAddress'
-import { BigNumber } from '@ethersproject/bignumber'
 import { Falsy } from '../model/types'
+import { useContractCall } from './useContractCall'
+import { BigNumber } from '@ethersproject/bignumber'
 
-export function useEtherBalance(address: string | Falsy) {
+export function useEtherBalance(address: string | Falsy): BigNumber | undefined {
   const multicallAddress = useMulticallAddress()
-  const getEthBalanceCall = address && MultiCallABI.encodeFunctionData('getEthBalance', [address])
-
-  const etherBalance = useChainCall(
-    getEthBalanceCall && multicallAddress && { address: multicallAddress, data: getEthBalanceCall }
-  )
-  return { etherBalance: etherBalance !== undefined ? BigNumber.from(etherBalance) : undefined }
+  const [etherBalance] = useContractCall(MultiCallABI, multicallAddress, 'getEthBalance', address && [address]) ?? []
+  return etherBalance
 }
