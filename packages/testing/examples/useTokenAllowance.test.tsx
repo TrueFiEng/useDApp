8c06358ff5a53da760f1bc3e300461e3f0abbe2f
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2021-03-23 07:25:09 +0100

    💲 Create useTokenAllowance hook (#81)
    * Add useTokenAllowance hook
    
    * Add useTokenAllowance docs
    
    Co-authored-by: Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>

---
+++ packages/testing/examples/useTokenAllowance.test.tsx
@@ -0,0 +1,50 @@
+import { MockProvider } from '@ethereum-waffle/provider'
+import { Contract } from '@ethersproject/contracts'
+import { useTokenAllowance } from '@usedapp/core'
+import chai, { expect } from 'chai'
+import { solidity } from 'ethereum-waffle'
+import { renderWeb3Hook } from '../src'
+import { deployMockToken } from '../src/utils/deployMockToken'
+import { utils } from 'ethers'
+
+chai.use(solidity)
+
+describe('useTokenAllowance', () => {
+  const mockProvider = new MockProvider()
+  const [deployer, spender] = mockProvider.getWallets()
+  let deployerToken: Contract
+
+  beforeEach(async () => {
+    deployerToken = await deployMockToken(deployer)
+  })
+
+  it('returns 0 when spender is not yet approved', async () => {
+    const { result, waitForCurrent } = await renderWeb3Hook(
+      () => useTokenAllowance(deployer.address, spender.address, deployerToken.address),
+      {
+        mockProvider,
+      }
+    )
+
+    await waitForCurrent((val) => val !== undefined)
+
+    expect(result.error).to.be.undefined
+    expect(result.current).to.eq(0)
+  })
+
+  it('returns current allowance', async () => {
+    await deployerToken.approve(spender.address, utils.parseEther('1'))
+
+    const { result, waitForCurrent } = await renderWeb3Hook(
+      () => useTokenAllowance(deployer.address, spender.address, deployerToken.address),
+      {
+        mockProvider,
+      }
+    )
+
+    await waitForCurrent((val) => val !== undefined)
+
+    expect(result.error).to.be.undefined
+    expect(result.current).to.eq(utils.parseEther('1'))
+  })
+})
