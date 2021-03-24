f469d3d880810fa110fe01ef8683a0f420f9523a
nezouse <stankiewicz1998@gmail.com>
2021-03-24 08:00:53 +0100

    Add compareAddress to addressHelpers

--- packages/core/src/helpers/address.ts
+++ packages/core/src/helpers/address.ts
@@ -1,7 +1,13 @@
 import { utils } from 'ethers'
+import { BigNumber } from '@ethersproject/bignumber'
 
 export function shortenAddress(address: string) {
   const formattedAddress = utils.getAddress(address)
 
   return formattedAddress.substring(0, 6) + '...' + formattedAddress.substring(formattedAddress.length - 4)
+}
+
+export function compareAddress(firstAddress: string, secondAddress: string) {
+  const isGreater = BigNumber.from(firstAddress).gte(BigNumber.from(secondAddress))
+  return isGreater ? 1 : -1
 }
--- packages/core/test/helpers/address.test.ts
+++ packages/core/test/helpers/address.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai'
-import { shortenAddress } from '../../src/helpers/address'
+import { shortenAddress, compareAddress } from '../../src/helpers/address'
 
 describe('addressHelper', () => {
   describe('shortenAddress', () => {
@@ -25,6 +25,36 @@ describe('addressHelper', () => {
       const address = "i'm not an address"
 
       expect(() => shortenAddress(address)).to.throw
+    })
+  })
+
+  describe('addressCompare', () => {
+    it('first bigger', () => {
+      const address1 = '0x24d53843ce280bbae7d47635039a94b471547fd5'
+      const address2 = '0x24d53843ce280bbae7d47635039a94b471547f00'
+
+      expect(compareAddress(address1, address2)).to.eq(1)
+    })
+
+    it('second bigger', () => {
+      const address1 = '0x004212440ad484f55997750cfae3e13ca1751283'
+      const address2 = '0xe24212440ad484f55997750cfae3e13ca1751283'
+
+      expect(compareAddress(address1, address2)).to.eq(-1)
+    })
+
+    it('both equal', () => {
+      const address1 = '0x8ea9f79f41a20c6a7c724c1d808f9946b7ed620b'
+      const address2 = '0x8ea9f79f41a20c6a7c724c1d808f9946b7ed620b'
+
+      expect(compareAddress(address1, address2)).to.eq(1)
+    })
+
+    it("can't parse address", () => {
+      const address1 = 'im not an address'
+      const address2 = '0xb293c3b2b4596824c57ad642ea2da4e146cca4cf'
+
+      expect(() => compareAddress(address1, address2)).to.throw
     })
   })
 })
