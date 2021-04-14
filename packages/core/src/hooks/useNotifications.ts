190c7445e8abe0a848243d175660948f17d6f6af
nezouse <stankiewicz1998@gmail.com>
2021-04-14 13:31:06 +0200

    Filter expired notifications in useNotifications

--- packages/core/src/hooks/useNotifications.ts
+++ packages/core/src/hooks/useNotifications.ts
@@ -1,10 +1,24 @@
 import { useMemo } from 'react'
-import { useNotificationsContext } from '../providers'
+import { Notification, useNotificationsContext } from '../providers'
 import { useEthers } from './useEthers'
+import { useInterval } from './useInterval'
 
-export function useNotifications() {
+interface NotificationsOptions {
+  checkInterval?: number
+  expirationPeriod?: number
+}
+
+function getExpiredNotifications(notifications: Notification[], expirationPeriod: number) {
+  const timeFromCreation = (creationTime: number) => Date.now() - creationTime
+
+  return notifications.filter((notification) => timeFromCreation(notification.submittedAt) >= expirationPeriod)
+}
+
+export function useNotifications(options?: NotificationsOptions) {
   const { chainId, account } = useEthers()
   const { addNotification, notifications, removeNotification } = useNotificationsContext()
+  const checkInterval = options?.checkInterval ?? 500
+  const expirationPeriod = options?.expirationPeriod ?? 5000
 
   const chainNotifications = useMemo(() => {
     if (chainId === undefined || !account) {
@@ -12,6 +26,17 @@ export function useNotifications() {
     }
     return notifications[chainId] ?? []
   }, [notifications, chainId, account])
+
+  useInterval(() => {
+    if (!chainId) {
+      return
+    }
+
+    const expiredNotification = getExpiredNotifications(chainNotifications, expirationPeriod)
+    for (const notification of expiredNotification) {
+      removeNotification({ notificationId: notification.id, chainId })
+    }
+  }, checkInterval)
 
   return {
     notifications: chainNotifications,
