6e75d5fb9873f0523bf24c62319384d4c95043c2
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2021-05-07 14:02:21 +0200

    Fix out of order requests

--- packages/core/src/providers/chainState/chainStateReducer.ts
+++ packages/core/src/providers/chainState/chainStateReducer.ts
@@ -1,6 +1,6 @@
 import { ChainState } from './model'
 
-interface State {
+export interface State {
   [chainId: number]:
     | {
         blockNumber: number
@@ -30,9 +30,24 @@ export function chainStateReducer(state: State = {}, action: Action) {
   const current = state[action.chainId]?.blockNumber
   if (!current || action.blockNumber >= current) {
     if (action.type === 'FETCH_SUCCESS') {
+      let newState = action.state
+      if (action.blockNumber === current) {
+        // merge with existing state to prevent requests coming out of order
+        // from overwriting the data
+        const oldState = state[action.chainId]?.state ?? {}
+        for (const [address, entries] of Object.entries(oldState)) {
+          newState = {
+            ...newState,
+            [address]: {
+              ...entries,
+              ...newState[address],
+            },
+          }
+        }
+      }
       return {
         ...state,
-        [action.chainId]: { blockNumber: action.blockNumber, state: action.state },
+        [action.chainId]: { blockNumber: action.blockNumber, state: newState },
       }
     } else if (action.type === 'FETCH_ERROR') {
       return {
