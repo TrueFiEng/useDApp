3833789cc522e13f7c839b7af6f977a373b62939
Just me <itsolumidesamson@gmail.com>
2021-05-11 22:16:37 +0100

    export useConfig

--- packages/core/src/providers/index.ts
+++ packages/core/src/providers/index.ts
@@ -1,6 +1,7 @@
 export * from './DAppProvider'
 export * from './blockNumber'
 export * from './chainState'
+export * from './config'
 export { useTransactionsContext } from './transactions/context'
 export { useNotificationsContext } from './notifications/context'
 export * from './transactions/model'
