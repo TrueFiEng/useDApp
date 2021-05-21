a0586f276110b31c91044be23c94549e702a2765
Szymon Szlachtowicz <szymon.szlachtowicz@Szymons-MacBook-Pro.local>
2021-05-21 13:00:16 +0200

    Fix useContractCall hook order

--- packages/core/src/hooks/useContractCall.ts
+++ packages/core/src/hooks/useContractCall.ts
@@ -2,6 +2,24 @@ import { Interface } from '@ethersproject/abi'
 import { useMemo } from 'react'
 import { Falsy } from '../model/types'
 import { useChainCall, useChainCalls } from './useChainCalls'
+import { ChainCall } from '../providers/chainState/callsReducer'
+
+function resolveCallData(call: ContractCall | Falsy): ChainCall | Falsy {
+  if (!call) {
+    return undefined
+  }
+
+  let callData = undefined
+  try {
+    callData = call.args.every((arg) => arg !== undefined) && call.abi.encodeFunctionData(call.method, call.args)
+  } catch {
+    console.warn(
+      `Invalid callData: address=${call && call.address} method=${call && call.method} args=${call && call.args}`
+    )
+  }
+
+  return callData && { address: call.address, data: callData }
+}
 
 export interface ContractCall {
   abi: Interface
@@ -11,10 +29,15 @@ export interface ContractCall {
 }
 
 export function useContractCall(call: ContractCall | Falsy): any[] | undefined {
-  const callData =
-    call && call.args.every((arg) => arg !== undefined) && call.abi.encodeFunctionData(call.method, call.args)
+  const result = useChainCall(resolveCallData(call))
 
-  const result = useChainCall(call && callData && { address: call.address, data: callData })
+  const memoReturn = useMemo(
+    () =>
+      call && result != '0x' && result !== undefined
+        ? (call.abi.decodeFunctionResult(call.method, result) as any[])
+        : undefined,
+    [result]
+  )
 
   if (result === '0x') {
     console.warn(
@@ -22,25 +45,17 @@ export function useContractCall(call: ContractCall | Falsy): any[] | undefined {
     )
     return undefined
   }
-
-  return useMemo(
-    () => (call && result !== undefined ? (call.abi.decodeFunctionResult(call.method, result) as any[]) : undefined),
-    [result]
-  )
+  return memoReturn
 }
 
 export function useContractCalls(calls: ContractCall[]): (any[] | undefined)[] {
-  const results = useChainCalls(
-    calls.map((call) => ({
-      address: call.address,
-      data: call.abi.encodeFunctionData(call.method, call.args),
-    }))
-  )
+  const filteredCalls = calls.map((call) => resolveCallData(call)).filter((call) => call) as ChainCall[]
+  const results = useChainCalls(filteredCalls)
 
   return useMemo(
     () =>
       results.map((result, idx) =>
-        result ? (calls[idx].abi.decodeFunctionResult(calls[idx].method, result) as any[]) : undefined
+        result != '0x' && result ? (calls[idx].abi.decodeFunctionResult(calls[idx].method, result) as any[]) : undefined
       ),
     [results]
   )
