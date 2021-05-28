3a9692261d57a95765e3300ddef68d26f1097a99
Szymon Szlachtowicz <szymon.szlachtowicz@Szymons-MacBook-Pro.local>
2021-05-28 15:16:14 +0200

    Add lodash to dev dependencies

--- packages/core/src/providers/config/reducer.ts
+++ packages/core/src/providers/config/reducer.ts
@@ -1,9 +1,6 @@
 import { FullConfig, Config } from '../..'
+import { merge } from 'lodash'
 
 export function configReducer(state: FullConfig, action: Config): FullConfig {
-  const readOnlyUrls = { ...state.readOnlyUrls, ...action.readOnlyUrls }
-  const multicallAddresses = { ...state.multicallAddresses, ...action.multicallAddresses }
-  const supportedChains = { ...state.supportedChains, ...action.supportedChains }
-  const notifications = { ...state.notifications, ...action.notifications }
-  return { ...state, ...action, readOnlyUrls, multicallAddresses, supportedChains, notifications }
+  return merge(state,action)
 }
