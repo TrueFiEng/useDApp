1b429a7a489bbf75e8fa22b06a92d127fdfa5c61
quag <4254570+quagliero@users.noreply.github.com>
2021-07-24 10:16:54 +0100

    fix: handle lookupAddress errors
    Networks like matic don't have ens lookup and throw errors on the application when checking

--- packages/core/src/hooks/useLookupAddress.ts
+++ packages/core/src/hooks/useLookupAddress.ts
@@ -3,17 +3,20 @@ import { useEthers } from './useEthers'
 
 export function useLookupAddress() {
   const { account, library } = useEthers()
-  const [ens, setEns] = useState<string>()
+  const [ens, setEns] = useState<string | null>()
 
   useEffect(() => {
     let mounted = true
 
     if (account && library) {
-      library?.lookupAddress(account).then((name) => {
-        if (mounted) {
-          setEns(name)
-        }
-      })
+      library
+        ?.lookupAddress(account)
+        .then((name) => {
+          if (mounted) {
+            setEns(name)
+          }
+        })
+        .catch(() => setEns(null))
     }
 
     return () => {
