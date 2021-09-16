a94fae5fd6c92e807dd0cdbbd24d97a2fa2a3bd0
Łukasz Stankiewicz <stankiewicz1998@gmail.com>
2021-09-16 20:11:40 +0200

    Add missing lodash-merge dependency (#323)
    * Add missing lodash-merge dependency

---
+++ .changeset/moody-cycles-think.md
@@ -0,0 +1,5 @@
+---
+"@usedapp/core": patch
+---
+
+Add missing lodash-merge dependency
--- packages/core/src/providers/config/reducer.ts
+++ packages/core/src/providers/config/reducer.ts
@@ -1,5 +1,5 @@
 import { FullConfig, Config } from '../..'
-import { merge } from 'lodash'
+import merge from 'lodash.merge'
 
 export function configReducer(state: FullConfig, action: Config): FullConfig {
   return merge({}, state, action)
