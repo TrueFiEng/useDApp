816c18fbc89383e57062e2145aa41900132c38f0
Pritesh K <37466934+priteshkeleven@users.noreply.github.com>
2021-10-10 07:13:41 +0530

    Updated currencies.ts after running prettier linting
    File wasn't formatted as @usedapps/core/prettier.config.js. I have run it again and updated file.

--- packages/core/src/constants/currencies.ts
+++ packages/core/src/constants/currencies.ts
@@ -11,7 +11,12 @@ export const BNB = new NativeCurrency('Binance Coin', 'BNB', ChainId.BSC)
 export const BUSD = new Token('Binance USD', 'BUSD', ChainId.BSC, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56')
 
 export const TestBNB = new NativeCurrency('Test Binance Coin', 'TBNB', ChainId.BSCTestnet)
-export const TestBUSD = new Token('Test Binance USD', 'TBUSD', ChainId.BSCTestnet, '0x8301F2213c0eeD49a7E28Ae4c3e91722919B8B47')
+export const TestBUSD = new Token(
+  'Test Binance USD',
+  'TBUSD',
+  ChainId.BSCTestnet,
+  '0x8301F2213c0eeD49a7E28Ae4c3e91722919B8B47'
+)
 
 export const NATIVE_CURRENCY = {
   [ChainId.Mainnet]: Ether,
