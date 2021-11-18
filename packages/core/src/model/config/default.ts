d5f5a3310cfa76881b22e20e1b35bfb69122438a
Fred Lin <gasolin@gmail.com>
2021-11-19 03:31:53 +0800

    [refactor] generate supportedChains

--- packages/core/src/model/config/default.ts
+++ packages/core/src/model/config/default.ts
@@ -1,40 +1,10 @@
-import { Localhost, Hardhat } from '../chain/local'
-import { Mainnet, Ropsten, Rinkeby, Goerli, Kovan } from '../chain/ethereum'
-import { BSC, BSCTestnet } from '../chain/bsc'
-import { xDai } from '../chain/xdai'
-import { Polygon, Mumbai } from '../chain/polygon'
-import { Theta, ThetaTestnet } from '../chain/theta'
-import { Moonriver } from '../chain/moonriver'
-import { Harmony } from '../chain/harmony'
-import { Palm } from '../chain/palm'
-import { Fantom } from '../chain/fantom'
-import { Avalanche } from '../chain/avalanche'
+import { SUPPORT_NETWORKS } from '../../constants/chainId'
 
 import { FullConfig } from './Config'
 
 export const DEFAULT_CONFIG: FullConfig = {
   pollingInterval: 15000,
-  supportedChains: [
-    Mainnet.chainId,
-    Ropsten.chainId,
-    Rinkeby.chainId,
-    Goerli.chainId,
-    Kovan.chainId,
-    BSC.chainId,
-    BSCTestnet.chainId,
-    xDai.chainId,
-    Localhost.chainId,
-    Hardhat.chainId,
-    Polygon.chainId,
-    Mumbai.chainId,
-    Theta.chainId,
-    ThetaTestnet.chainId,
-    Harmony.chainId,
-    Palm.chainId,
-    Moonriver.chainId,
-    Fantom.chainId,
-    Avalanche.chainId,
-  ],
+  supportedChains: SUPPORT_NETWORKS.map(network => network.chainId),
   notifications: {
     checkInterval: 500,
     expirationPeriod: 5000,
