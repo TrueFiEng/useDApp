4d616fa1346365eaeaabd58238026071e66c1fcf
rzadp <roopert7@gmail.com>
2021-11-20 18:44:10 +0100

    Split it

--- packages/example-backend/src/dAppService.ts
+++ packages/example-backend/src/dAppService.ts
@@ -1,179 +1,25 @@
 import {
-  ChainId,
-  Config,
-  useBlockMeta,
-  useBlockNumber,
-  useEtherBalance,
-  useTokenBalance,
   ChainCall,
-  MultiCallABI,
-  GET_CURRENT_BLOCK_TIMESTAMP_CALL,
+  ContractCall,
+  ERC20Interface,
   GET_CURRENT_BLOCK_DIFFICULTY_CALL,
+  GET_CURRENT_BLOCK_TIMESTAMP_CALL,
+  MultiCallABI,
   parseDifficulty,
   parseTimestamp,
-  ChainState,
-  ContractCall,
-  encodeCallData,
-  ERC20Interface,
-  multicall,
-  getUnique
+  useBlockMeta,
+  useEtherBalance,
+  useTokenBalance
 } from '@usedapp/core'
-import { JsonRpcProvider } from '@ethersproject/providers'
-import { State } from 'reactive-properties'
-import { synchronized } from '@dxos/async'
-import { latch } from './util'
+import { DAppServiceBase, emptyChainCallResult, OnUpdate, ChainCallResult } from './dAppServiceBase'
 
-export type BlockNumber = ReturnType<typeof useBlockNumber>
 export type BlockTimestamp = ReturnType<typeof useBlockMeta>['timestamp']
 export type BlockDifficulty = ReturnType<typeof useBlockMeta>['difficulty']
 export type EtherBalance = ReturnType<typeof useEtherBalance>
 export type TokenBalance = ReturnType<typeof useTokenBalance>
 
-export type OnUpdate<T> = (newValue: T) => void | Promise<void>
-export interface UseChainCallResult<T = string | undefined> {
-  unsubscribe: () => void
-  value: undefined | Promise<T>
-}
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const emptyChainCallResult: UseChainCallResult<any> = { unsubscribe: () => {}, value: undefined }
-
-export class DAppService {
-  private _lastProcessedBlock: number | undefined = undefined
-  private _lastProcessedCalls: string | undefined = undefined
-  private _web3Provider: JsonRpcProvider
-  private _unsubscribe: (() => void) | undefined
-
-  private _blockNumberState = new State<BlockNumber>(undefined)
-  private _chainState = new State<ChainState>({})
-  private _chainCalls = new State<ChainCall[]>([])
-
-  constructor(private config: Config, private chainId: ChainId) {
-    if (!this.config.readOnlyUrls?.[chainId]) {
-      throw new Error(`Missing URL for chainId "${chainId}".`)
-    }
-    this._web3Provider = new JsonRpcProvider(this.config.readOnlyUrls[chainId], 'any')
-  }
-
-  get multicallAddress() {
-    return this.config.multicallAddresses?.[this.chainId]
-  }
-
-  get blockNumber() {
-    return this._blockNumberState.get()
-  }
-
-  useBlockNumber(onUpdate: (blockNumber: BlockNumber) => void) {
-    return this._blockNumberState.subscribe(() => onUpdate(this.blockNumber))
-  }
-
-  start() {
-    if (this._unsubscribe) return // Already started.
-    const update = (blockNumber: number) => this._blockNumberState.set(blockNumber)
-    this._web3Provider.on('block', update)
-
-    const unsubscribeBlockNumber = this._blockNumberState.subscribe(() => this.refresh()) // Refresh on block number change.
-    const unsubscribeCalls = this._chainCalls.subscribe(() => this.refresh()) // Refresh on a change in a list of calls.
-    this._unsubscribe = () => {
-      unsubscribeBlockNumber()
-      unsubscribeCalls()
-      this._web3Provider.off('block', update)
-    }
-  }
-
-  stop() {
-    this._unsubscribe?.()
-    this._unsubscribe = undefined
-  }
-
-  @synchronized
-  private async refresh() {
-    try {
-      const blockNumber = this.blockNumber
-      if (!blockNumber || !this.multicallAddress) return
-      const chainCalls = this._chainCalls.get()
-      const uniqueCalls = getUnique(chainCalls)
-      if ((this._lastProcessedBlock ?? 0 >= blockNumber) && this._lastProcessedCalls === JSON.stringify(uniqueCalls)) {
-        // The state for this block number and exactly these calls has already been updated.
-        return
-      }
-
-      const newState = await multicall(this._web3Provider, this.multicallAddress, blockNumber, uniqueCalls)
-      this._chainState.set(newState)
-
-      this._lastProcessedBlock = blockNumber
-      this._lastProcessedCalls = JSON.stringify(uniqueCalls)
-    } catch (e) {
-      console.error('Refresh failed.')
-      console.error(e)
-    }
-  }
-
-  protected chainState(address: string | undefined, data: string) {
-    if (!address) return undefined
-    return this._chainState.get()[address]?.[data]
-  }
-
-  protected useChainState(address: string | undefined, data: string, onUpdate: OnUpdate<string | undefined>) {
-    if (!address) return () => {} // eslint-disable-line @typescript-eslint/no-empty-function
-    return this._chainState.subscribe(() => onUpdate(this.chainState(address, data)))
-  }
-
-  @synchronized
-  protected addCall(call: ChainCall) {
-    this._chainCalls.set([...this._chainCalls.get(), call])
-  }
-
-  @synchronized
-  protected removeCall(call: ChainCall) {
-    const chainCalls = this._chainCalls.get()
-    const index = chainCalls.findIndex(x => x.address === call.address && x.data === call.data)
-    if (index !== -1) {
-      this._chainCalls.set(chainCalls.filter((_, i) => i !== index))
-    }
-  }
-
-  protected useChainCall(call: ChainCall, onUpdate?: OnUpdate<string | undefined>): UseChainCallResult {
-    const [value, setValue] = latch<string | undefined>()
-    this.addCall(call)
-
-    const unsub = this.useChainState(call.address, call.data, result => {
-      if (result !== undefined) {
-        setValue(result)
-        onUpdate?.(result)
-      }
-    })
-
-    return {
-      unsubscribe: () => {
-        unsub()
-        this.removeCall(call)
-      },
-      value
-    }
-  }
-
-  protected useContractCall<T>(contractCall: ContractCall, onUpdate?: OnUpdate<T>): UseChainCallResult<T> {
-    const [value, setValue] = latch<T>()
-    const call = encodeCallData(contractCall)
-    if (!call) return emptyChainCallResult
-
-    const { unsubscribe } = this.useChainCall(call, result => {
-      const decoded: T | undefined = result
-        ? contractCall.abi.decodeFunctionResult(contractCall.method, result)[0]
-        : undefined
-      if (decoded !== undefined) {
-        setValue(decoded)
-        onUpdate?.(decoded)
-      }
-    })
-
-    return {
-      unsubscribe,
-      value
-    }
-  }
-
-  useBlockTimestamp(onUpdate?: OnUpdate<BlockTimestamp>): UseChainCallResult<BlockTimestamp> {
+export class DAppService extends DAppServiceBase {
+  useBlockTimestamp(onUpdate?: OnUpdate<BlockTimestamp>): ChainCallResult<BlockTimestamp> {
     if (!this.multicallAddress) return emptyChainCallResult
     const call: ChainCall = {
       address: this.multicallAddress,
@@ -190,7 +36,7 @@ export class DAppService {
     }
   }
 
-  useBlockDifficulty(onUpdate?: OnUpdate<BlockDifficulty>): UseChainCallResult<BlockDifficulty> {
+  useBlockDifficulty(onUpdate?: OnUpdate<BlockDifficulty>): ChainCallResult<BlockDifficulty> {
     if (!this.multicallAddress) return emptyChainCallResult
     const call: ChainCall = {
       address: this.multicallAddress,
@@ -207,7 +53,7 @@ export class DAppService {
     }
   }
 
-  useEtherBalance(address: string, onUpdate?: OnUpdate<EtherBalance>): UseChainCallResult<EtherBalance> {
+  useEtherBalance(address: string, onUpdate?: OnUpdate<EtherBalance>): ChainCallResult<EtherBalance> {
     if (!this.multicallAddress) return emptyChainCallResult
     const contractCall: ContractCall = {
       abi: MultiCallABI,
@@ -223,7 +69,7 @@ export class DAppService {
     tokenAddress: string,
     address: string,
     onUpdate?: OnUpdate<TokenBalance>
-  ): UseChainCallResult<TokenBalance> {
+  ): ChainCallResult<TokenBalance> {
     if (!this.multicallAddress) return emptyChainCallResult
     const contractCall: ContractCall = {
       abi: ERC20Interface,
---
+++ packages/example-backend/src/dAppServiceBase.ts
@@ -0,0 +1,162 @@
+import { synchronized } from '@dxos/async'
+import { JsonRpcProvider } from '@ethersproject/providers'
+import {
+  ChainCall,
+  ChainId,
+  ChainState,
+  Config,
+  ContractCall,
+  encodeCallData,
+  getUnique,
+  multicall,
+  useBlockNumber
+} from '@usedapp/core'
+import { State } from 'reactive-properties'
+import { latch } from './util'
+
+export type BlockNumber = ReturnType<typeof useBlockNumber>
+
+export type OnUpdate<T> = (newValue: T) => void | Promise<void>
+export interface ChainCallResult<T = string | undefined> {
+  unsubscribe: () => void
+  value: undefined | Promise<T>
+}
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export const emptyChainCallResult: ChainCallResult<any> = { unsubscribe: () => {}, value: undefined }
+
+export class DAppServiceBase {
+  private _lastProcessedBlock: number | undefined = undefined
+  private _lastProcessedCalls: string | undefined = undefined
+  private _web3Provider: JsonRpcProvider
+  private _unsubscribe: (() => void) | undefined
+
+  private _blockNumberState = new State<BlockNumber>(undefined)
+  private _chainState = new State<ChainState>({})
+  private _chainCalls = new State<ChainCall[]>([])
+
+  constructor(private config: Config, private chainId: ChainId) {
+    if (!this.config.readOnlyUrls?.[chainId]) {
+      throw new Error(`Missing URL for chainId "${chainId}".`)
+    }
+    this._web3Provider = new JsonRpcProvider(this.config.readOnlyUrls[chainId], 'any')
+  }
+
+  get multicallAddress() {
+    return this.config.multicallAddresses?.[this.chainId]
+  }
+
+  get blockNumber() {
+    return this._blockNumberState.get()
+  }
+
+  useBlockNumber(onUpdate: (blockNumber: BlockNumber) => void) {
+    return this._blockNumberState.subscribe(() => onUpdate(this.blockNumber))
+  }
+
+  start() {
+    if (this._unsubscribe) return // Already started.
+    const update = (blockNumber: number) => this._blockNumberState.set(blockNumber)
+    this._web3Provider.on('block', update)
+
+    const unsubscribeBlockNumber = this._blockNumberState.subscribe(() => this.refresh()) // Refresh on block number change.
+    const unsubscribeCalls = this._chainCalls.subscribe(() => this.refresh()) // Refresh on a change in a list of calls.
+    this._unsubscribe = () => {
+      unsubscribeBlockNumber()
+      unsubscribeCalls()
+      this._web3Provider.off('block', update)
+    }
+  }
+
+  stop() {
+    this._unsubscribe?.()
+    this._unsubscribe = undefined
+  }
+
+  @synchronized
+  private async refresh() {
+    try {
+      const blockNumber = this.blockNumber
+      if (!blockNumber || !this.multicallAddress) return
+      const chainCalls = this._chainCalls.get()
+      const uniqueCalls = getUnique(chainCalls)
+      if ((this._lastProcessedBlock ?? 0 >= blockNumber) && this._lastProcessedCalls === JSON.stringify(uniqueCalls)) {
+        // The state for this block number and exactly these calls has already been updated.
+        return
+      }
+
+      const newState = await multicall(this._web3Provider, this.multicallAddress, blockNumber, uniqueCalls)
+      this._chainState.set(newState)
+
+      this._lastProcessedBlock = blockNumber
+      this._lastProcessedCalls = JSON.stringify(uniqueCalls)
+    } catch (e) {
+      console.error('Refresh failed.')
+      console.error(e)
+    }
+  }
+
+  protected chainState(address: string | undefined, data: string) {
+    if (!address) return undefined
+    return this._chainState.get()[address]?.[data]
+  }
+
+  protected useChainState(address: string | undefined, data: string, onUpdate: OnUpdate<string | undefined>) {
+    if (!address) return () => {} // eslint-disable-line @typescript-eslint/no-empty-function
+    return this._chainState.subscribe(() => onUpdate(this.chainState(address, data)))
+  }
+
+  @synchronized
+  protected addCall(call: ChainCall) {
+    this._chainCalls.set([...this._chainCalls.get(), call])
+  }
+
+  @synchronized
+  protected removeCall(call: ChainCall) {
+    const chainCalls = this._chainCalls.get()
+    const index = chainCalls.findIndex(x => x.address === call.address && x.data === call.data)
+    if (index !== -1) {
+      this._chainCalls.set(chainCalls.filter((_, i) => i !== index))
+    }
+  }
+
+  protected useChainCall(call: ChainCall, onUpdate?: OnUpdate<string | undefined>): ChainCallResult {
+    const [value, setValue] = latch<string | undefined>()
+    this.addCall(call)
+
+    const unsub = this.useChainState(call.address, call.data, result => {
+      if (result !== undefined) {
+        setValue(result)
+        onUpdate?.(result)
+      }
+    })
+
+    return {
+      unsubscribe: () => {
+        unsub()
+        this.removeCall(call)
+      },
+      value
+    }
+  }
+
+  protected useContractCall<T>(contractCall: ContractCall, onUpdate?: OnUpdate<T>): ChainCallResult<T> {
+    const [value, setValue] = latch<T>()
+    const call = encodeCallData(contractCall)
+    if (!call) return emptyChainCallResult
+
+    const { unsubscribe } = this.useChainCall(call, result => {
+      const decoded: T | undefined = result
+        ? contractCall.abi.decodeFunctionResult(contractCall.method, result)[0]
+        : undefined
+      if (decoded !== undefined) {
+        setValue(decoded)
+        onUpdate?.(decoded)
+      }
+    })
+
+    return {
+      unsubscribe,
+      value
+    }
+  }
+}
