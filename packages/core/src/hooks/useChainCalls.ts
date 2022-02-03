f8c4f0e7de57f3fb0c23287d98af66578379c547
Hubert Nastały <h.nastaly@gmail.com>
2022-02-03 11:23:35 +0100

    Add deprecation for useChainCalls and useChainCall

--- packages/core/src/hooks/useChainCalls.ts
+++ packages/core/src/hooks/useChainCalls.ts
@@ -2,11 +2,17 @@ import { RawCall } from '../providers'
 import { Falsy } from '../model/types'
 import { useRawCalls } from './useRawCalls'
 
+/**
+ * @deprecated It's recommended to use useRawCalls instead
+ */
 export function useChainCalls(calls: (RawCall | Falsy)[]) {
   const results = useRawCalls(calls)
   return results.map((result) => result?.value)
 }
 
+/**
+ * @deprecated It's recommended to use useRawCall instead
+ */
 export function useChainCall(call: RawCall | Falsy) {
   return useChainCalls([call])[0]
 }
