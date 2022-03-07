26b0b0126ab042b2907578a43de332167c5b4020
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2022-03-07 16:25:43 +0100

    🐼 Update project id (#557)

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -6,7 +6,7 @@ import { App } from './App'
 const config = {
   readOnlyChainId: Mainnet.chainId,
   readOnlyUrls: {
-    [Mainnet.chainId]: 'https://mainnet.infura.io/v3/14a0951f47e646c1b241aa533e150219',
+    [Mainnet.chainId]: 'https://mainnet.infura.io/v3/57fc2c19095745e59ab96a4aa87dada8',
   },
   multicallVersion: 2 as const,
 }
