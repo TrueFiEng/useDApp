d8229b96c6634d743b0b1ea30dbc4cc90db33939
Przemyslaw Rzad <roopert7@gmail.com>
2022-03-09 12:57:28 +0100

    🦖 Introduce Docusaurus documentation site (#559)
    * Auto-generate docosaurus template
    
    * Nohoist
    
    Try moving docs out of workspaces
    
    * Noopen
    
    * Start introducing content
    
    * More progress
    
    * Add a netlify job
    
    * Remove unused file
    
    * Typo
    
    * typo
    
    * typo
    
    Co-authored-by: Marik-D <maretskii@gmail.com>

---
+++ packages/docs/src/pages/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title} Documentation</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p>
+          Framework for rapid Dapp development.<br/>
+          Simple. Robust. Extendable. Testable.
+        </p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/">
+            Get Started
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`${siteConfig.title} Documentation`}
+      description="Framework for rapid DApp development.">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
