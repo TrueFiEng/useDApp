bcccbce278cc8652f9b4a4fd513467fcca90eb3e
yivlad <jacenko.vlad@gmail.com>
2022-03-11 11:53:25 +0100

    Add include your chain in config step

--- packages/core/src/constants/type/Config.ts
+++ packages/core/src/constants/type/Config.ts
@@ -9,20 +9,49 @@ export type MulticallAddresses = {
 }
 
 export type FullConfig = {
+  /**
+   * ChainId of a chain you want to connect to by default in a read-only mode
+   */
   readOnlyChainId?: number
+  /**
+   * Mapping of ChainId's to node URLs to use in read-only mode.
+   */
   readOnlyUrls?: NodeUrls
+  /**
+   * Mapping of ChainId's to multicall contract addresses on the chain.
+   */
   multicallAddresses?: MulticallAddresses
+  /**
+   * Version of multicall contract on the chain.
+   */
   multicallVersion: 1 | 2
+  /**
+   * @deprecated
+   */
   supportedChains?: number[]
+  /**
+   * List of intended supported chain configs. 
+   * If a user tries to connect to an unsupported chain an error value will be returned by useEthers.
+   * @default DEFAULT_SUPPORTED_CHAINS
+   */
   networks?: Chain[]
+  /**
+   * Polling interval for a new block.
+   */
   pollingInterval: number
   notifications: {
     checkInterval: number
     expirationPeriod: number
   }
+  /**
+   * Paths to locations in local storage
+   */
   localStorage: {
     transactionPath: string
   }
+  /**
+   * Enables reconnecting to last used provider when user revisits the page.
+   */
   autoConnect: boolean
 }
 
