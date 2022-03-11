4125ae30f92bd27929ceb6a4c439d3bc5b527882
yivlad <jacenko.vlad@gmail.com>
2022-03-11 12:27:08 +0100

    Revert changes to example app

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,27 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Mainnet, DAppProvider, Ropsten, Kovan, Config, Arbitrum, DEFAULT_SUPPORTED_CHAINS, Chain } from '@usedapp/core'
+import { Mainnet, DAppProvider, Ropsten, Kovan, Config, Arbitrum } from '@usedapp/core'
 import { App } from './App'
 
-export const TutorialChain: Chain = {
-  chainId: 99999,
-  chainName: 'TutorialChain',
-  isTestChain: false,
-  isLocalChain: false,
-  multicallAddress: '0x0000000000000000000000000000000000000000',
-  getExplorerAddressLink: (address: string) => `https://tutorialchain.etherscan.io/address/${address}`,
-  getExplorerTransactionLink: (transactionHash: string) => `https://tutorialchain.etherscan.io/tx/${transactionHash}`,
-}
-
 const config: Config = {
-  readOnlyChainId: TutorialChain.chainId,
+  readOnlyChainId: Mainnet.chainId,
   readOnlyUrls: {
     [Mainnet.chainId]: 'https://mainnet.infura.io/v3/57fc2c19095745e59ab96a4aa87dada8',
     [Ropsten.chainId]: 'https://ropsten.infura.io/v3/57fc2c19095745e59ab96a4aa87dada8',
     [Kovan.chainId]: 'https://kovan.infura.io/v3/57fc2c19095745e59ab96a4aa87dada8',
     [Arbitrum.chainId]: 'https://arb1.arbitrum.io/rpc',
   },
-  networks: [...DEFAULT_SUPPORTED_CHAINS, TutorialChain],
   multicallVersion: 2 as const,
 }
 
