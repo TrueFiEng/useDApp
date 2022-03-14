5a06eb2a25e32fdac7c5f9448bae95d1c08d403d
mateusz <mistrzumati@spoko.pl>
2022-03-14 09:27:50 +0100

    Add useToken test

---
+++ packages/core/src/hooks/useToken.test.tsx
@@ -0,0 +1,30 @@
+import { MockProvider } from '@ethereum-waffle/provider'
+import { Contract } from '@ethersproject/contracts'
+import { useToken } from '..'
+import { expect } from 'chai'
+import { renderWeb3Hook, deployMockToken, MOCK_TOKEN_INITIAL_BALANCE } from '../testing'
+
+describe('useToken', () => {
+  const mockProvider = new MockProvider()
+  const [deployer] = mockProvider.getWallets()
+  let token: Contract
+  const mockTokenData = {
+    name: 'MOCKToken',
+    symbol: 'MOCK',
+    decimals: 18,
+    totalSupply: MOCK_TOKEN_INITIAL_BALANCE
+  }
+
+  beforeEach(async () => {
+    token = await deployMockToken(deployer)
+  })
+
+  it('returns correct token constants', async () => {
+    const { result, waitForCurrent } = await renderWeb3Hook(() => useToken(token.address), {
+      mockProvider,
+    })
+    await waitForCurrent((val) => val !== undefined)
+    expect(result.error).to.be.undefined
+    expect(result.current).to.deep.equal(mockTokenData)
+  })
+})
