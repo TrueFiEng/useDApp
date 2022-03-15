ed01e2d6aa43215a551470766fb221ea775c7f42
rzadp <roopert7@gmail.com>
2022-03-15 16:11:48 +0100

    Import loader

---
+++ packages/docs/loader.js
@@ -0,0 +1,44 @@
+const { promises: fs } = require('fs');
+const path = require('path');
+
+/**
+ * @type {import('webpack').loader.Loader}
+ */
+module.exports = async function demoLoader() {
+  const files = await fs.readdir(path.dirname(this.resourcePath));
+  const codeFiles = files.filter((fileName) => fileName.includes('.js') || fileName.includes('.tsx') || fileName.includes('.tsx'));
+  let rawContent = {
+    js: null,
+    ts: null
+  }
+  let component = null;
+  let pageId = null;
+  await Promise.all(codeFiles.map(async (fileName) => {
+    const moduleID = fileName;
+    const moduleFilepath = path.join(
+      this.context,
+      moduleID
+    );
+    this.addDependency(moduleFilepath);
+    // TODO(wittjosiah): Load optional preview snippet as well.
+    // TODO(wittjosiah): Load TS and automatically transpile for JS.
+    if (moduleID.includes('.js')) {
+      rawContent.js = await fs.readFile(moduleFilepath, { encoding: 'utf-8' });
+    }
+    if (moduleID.includes('.ts') || moduleID.includes('.tsx')) {
+      rawContent.ts = await fs.readFile(moduleFilepath, { encoding: 'utf-8' });
+      component = `() => {
+        return require(${JSON.stringify("./" + moduleID)});
+      }`;
+      pageId = moduleID;
+    }
+  }));
+
+  const transformed = `
+  export const component = ${component};
+  export const rawContent = ${JSON.stringify(rawContent, null, 2)};
+  `;
+
+  // WARNING: Make sure the returned code is compatible with our .browserslistrc.
+  return transformed;
+};
