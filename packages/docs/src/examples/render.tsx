388f4937c93f8354b568e211ae37fd15077e384d
rzadp <roopert7@gmail.com>
2022-03-16 11:38:42 +0100

    New way of rendering

--- packages/docs/src/examples/render.tsx
+++ packages/docs/src/examples/render.tsx
@@ -1,20 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const originalRender: ReactDOM.Renderer = ReactDOM.render.bind(ReactDOM);
 (ReactDOM as any).render = () => {
   // Not rendering anything, overriding default behaviour of App examples.
 }
-
-export const render = (Component, root) => {
-  const intervalId = setInterval(() => {
-    const rootElement = document.getElementById(root);
-    if (rootElement) {
-      clearInterval(intervalId);
-      originalRender(<Component />, rootElement);
-    }
-  }, 200)
-  return null;
-}
-
-export default render;
