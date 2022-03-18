7e32254cf58b5a379501e27bda7ac249c9faf592
rzadp <roopert7@gmail.com>
2022-03-18 13:20:14 +0100

    Add define plugin

--- packages/docs/plugins/webpack-plugin.js
+++ packages/docs/plugins/webpack-plugin.js
@@ -1,6 +1,7 @@
 // const webpack = require("webpack");
 const path = require('path');
 const webpack = require('webpack');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 
 module.exports = function (context, options) {
   return {
@@ -8,6 +9,9 @@ module.exports = function (context, options) {
     configureWebpack(config, isServer) {
       return {
         plugins: [
+          new webpack.DefinePlugin({
+            "process.env.NODE_DEBUG": JSON.stringify(process.env.NODE_DEBUG)
+          }),
           new webpack.ProvidePlugin({
             Buffer: [require.resolve('buffer/'), 'Buffer']
           })
