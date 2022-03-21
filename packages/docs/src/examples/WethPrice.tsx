36d3d343c46b57cd9afb10828d806980774ea88c
mateusz <mateusz.jandula@trusttoken.com>
2022-03-21 11:02:11 +0100

    Add prices examples

---
+++ packages/docs/src/examples/WethPrice.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { useCoingeckoTokenPrice } from '@usedapp/coingecko'
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
+
+export function App()  {
+    const WETH_CONTRACT = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+    const wethPrice = useCoingeckoTokenPrice(WETH_CONTRACT, 'usd')
+
+    return (<p>Wrapped ether price: {wethPrice} $</p>)
+}
