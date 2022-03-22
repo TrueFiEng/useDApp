895c5515e9bfdd0bd9fc4727cfa0ce74e436942f
titandac <94769774+titandac@users.noreply.github.com>
2022-03-22 16:03:11 +0100

    🦎 Support metis mainnet (#484)
    * support metis mainnet
    
    * Update CHANGELOG.md
    
    Co-authored-by: titansdac <u16_0xffff@protonmail.com>
    Co-authored-by: Mateusz Janduła <71319308+mj426382@users.noreply.github.com>
    Co-authored-by: mateusz <mateusz.jandula@trusttoken.com>

--- packages/core/src/model/chain/metis.ts
+++ packages/core/src/model/chain/metis.ts
@@ -6,8 +6,18 @@ export const Stardust: Chain = {
   isTestChain: true,
   isLocalChain: false,
   multicallAddress: '0xaF9D4DC0698d8FD9f41387ecb08D9976079B8086',
-  getExplorerAddressLink: (address: string) => `https://stardust-explorer.metis.io/${address}`,
-  getExplorerTransactionLink: (transactionHash: string) => `https://stardust-explorer.metis.io/${transactionHash}`,
+  getExplorerAddressLink: (address: string) => `https://stardust-explorer.metis.io/address/${address}`,
+  getExplorerTransactionLink: (transactionHash: string) => `https://stardust-explorer.metis.io/tx/${transactionHash}`,
 }
 
-export default { Stardust }
+export const Andromeda: Chain = {
+  chainId: 1088,
+  chainName: 'Andromeda',
+  isTestChain: false,
+  isLocalChain: false,
+  multicallAddress: '0x1a2AFb22B8A90A77a93e80ceA61f89D04e05b796',
+  getExplorerAddressLink: (address: string) => `https://andromeda-explorer.metis.io/address/${address}`,
+  getExplorerTransactionLink: (transactionHash: string) => `https://andromeda-explorer.metis.io/tx/${transactionHash}`,
+}
+
+export default { Stardust, Andromeda }
