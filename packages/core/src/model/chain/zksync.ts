8ae7fefcb453105a1e2ff550353b812524354171
mateusz <mateusz.jandula@trusttoken.com>
2022-04-04 10:53:21 +0200

    Add multicall address for zksync testnet

--- packages/core/src/model/chain/zksync.ts
+++ packages/core/src/model/chain/zksync.ts
@@ -5,7 +5,7 @@ export const ZkSyncTestnet: Chain = {
   chainName: 'zkSync alpha testnet',
   isTestChain: true,
   isLocalChain: false,
-  multicallAddress: '0x11ce4B23bD875D7F5C6a31084f55fDe1e9A87507',
+  multicallAddress: '0x5014a961801de9a52548068bDac853CE337221e7',
   getExplorerAddressLink: (address: string) => `https://zksync2-testnet.zkscan.io/address/${address}`,
   getExplorerTransactionLink: (transactionHash: string) => `https://zksync2-testnet.zkscan.io/tx/${transactionHash}`,
 }
