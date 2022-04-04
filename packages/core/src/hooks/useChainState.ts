b8ba29fc5c76ef8cac84b36a9a17893de55ac246
rzadp <roopert7@gmail.com>
2022-04-04 10:28:02 +0200

    Read-only

--- packages/core/src/hooks/useChainState.ts
+++ packages/core/src/hooks/useChainState.ts
@@ -1,14 +1,15 @@
 import { useContext } from 'react'
 import { QueryParams } from '../constants/type/QueryParams'
-import { Action, MultiChainStatesContext, SingleChainState, useNetwork } from '../providers'
+import { Action, MultiChainStatesContext, SingleChainState, useConfig, useNetwork } from '../providers'
 
 export function useChainState(
   queryParams: QueryParams = {}
 ): (Partial<SingleChainState> & { dispatchCalls: (action: Action) => void }) | undefined {
   const multiChainState = useContext(MultiChainStatesContext)
+  const { readOnlyChainId } = useConfig()
 
   const { network } = useNetwork()
-  const chainId = queryParams.chainId ?? network.chainId
+  const chainId = queryParams.chainId ?? network.chainId ?? readOnlyChainId
 
   if (chainId === undefined) {
     return undefined
