0cc0e48f13bb0396c484d31030e9d37b044bddec
Marik-D <maretskii@gmail.com>
2022-04-06 11:18:07 +0200

    Make exports from testing and internal point to compiled code

---
+++ packages/core/internal.d.ts
@@ -0,0 +1,4 @@
+// This file is a workaround to provide types for imports from "@usedapp/core/internal".
+// We need to have it until https://github.com/microsoft/TypeScript/issues/33079 is closed.
+
+export * from './dist/esm/src/internal'
---
+++ packages/core/testing.d.ts
@@ -0,0 +1,4 @@
+// This file is a workaround to provide types for imports from "@usedapp/core/testing".
+// We need to have it until https://github.com/microsoft/TypeScript/issues/33079 is closed.
+
+export * from './dist/esm/src/testing'
