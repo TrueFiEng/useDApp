aadb00e41ba079298c8e1414cc57007de4ca3ff7
Marik-D <maretskii@gmail.com>
2022-04-08 16:21:52 +0200

    Lint

--- packages/core/src/hooks/useContractCall.ts
+++ packages/core/src/hooks/useContractCall.ts
@@ -61,7 +61,9 @@ export function useContractCalls(
   const rawCalls = useMemo(
     () => calls.map((call) => (chainId !== undefined ? encodeCallData(call, chainId) : undefined)),
     [
-      JSON.stringify(calls.map((call) => call && { address: call.address.toLowerCase(), method: call.method, args: call.args })),
+      JSON.stringify(
+        calls.map((call) => call && { address: call.address.toLowerCase(), method: call.method, args: call.args })
+      ),
       chainId,
     ]
   )
