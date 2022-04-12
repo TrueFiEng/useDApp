3f0528f98b3655028422da3f53100768a7abcc0f
rzadp <roopert7@gmail.com>
2022-04-12 16:33:10 +0200

    Try installing playwright deps

--- packages/example/playwright/metamask/constants.ts
+++ packages/example/playwright/metamask/constants.ts
@@ -8,4 +8,5 @@ const pathToExtension = path.join(__dirname, 'metamask-chrome-10.12.4')
 export const metamaskChromeArgs = [
   `--disable-extensions-except=${pathToExtension}`,
   `--load-extension=${pathToExtension}`,
+  '--disable-gpu'
 ]
