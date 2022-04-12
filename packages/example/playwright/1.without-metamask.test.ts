ae296cc1e0300b1ffc25bd3032e55bb84d6af0c6
rzadp <roopert7@gmail.com>
2022-04-12 16:35:18 +0200

    Test it

--- packages/example/playwright/1.without-metamask.test.ts
+++ packages/example/playwright/1.without-metamask.test.ts
@@ -1,35 +1,34 @@
 import { expect } from 'chai'
-import { Browser, BrowserContext, chromium, Page } from 'playwright'
+import { Browser, BrowserContext, chromium as browserType, Page } from 'playwright'
 import waitForExpect from 'wait-for-expect'
 import { baseUrl, headless, slowMo, XPath } from './utils'
 import { addPageDiagnostics } from './utils/pageDiagnostics'
-;[chromium].forEach((browserType) => {
-  describe(`Browser: ${browserType.name()} without Metamask`, () => {
-    let page: Page
-    let browser: Browser
-    let context: BrowserContext
 
-    const resetBrowserContext = async () => {
-      if (page) await page.close()
-      if (context) await context.close()
-      if (browser) await browser.close()
+describe(`Browser: ${browserType.name()} without Metamask`, () => {
+  let page: Page
+  let browser: Browser
+  let context: BrowserContext
 
-      browser = await browserType.launch({ headless, slowMo })
-      context = await browser.newContext()
-      page = await context.newPage()
-      addPageDiagnostics(page)
-    }
+  const resetBrowserContext = async () => {
+    if (page) await page.close()
+    if (context) await context.close()
+    if (browser) await browser.close()
 
-    before(resetBrowserContext)
-    after(() => browser?.close())
+    browser = await browserType.launch({ headless, slowMo })
+    context = await browser.newContext()
+    page = await context.newPage()
+    addPageDiagnostics(page)
+  }
 
-    describe('Balance', () => {
-      it('Reads the ETH2 staking contract', async () => {
-        await page.goto(`${baseUrl}balance`)
+  before(resetBrowserContext)
+  after(() => browser?.close())
 
-        await waitForExpect(async () => {
-          expect(await page.isVisible(XPath.text('span', 'ETH2 staking contract holds:'))).to.be.true
-        })
+  describe('Balance', () => {
+    it('Reads the ETH2 staking contract', async () => {
+      await page.goto(`${baseUrl}balance`)
+
+      await waitForExpect(async () => {
+        expect(await page.isVisible(XPath.text('span', 'ETH2 staking contract holds:'))).to.be.true
       })
     })
   })
