9a8a2b0f32c3f2f5c198b7ec7ff6060ee5ee24ab
mateusz <mateusz.jandula@trusttoken.com>
2022-04-14 12:11:14 +0200

    Fix lint

--- packages/siwe/src/requests.tsx
+++ packages/siwe/src/requests.tsx
@@ -1,66 +1,66 @@
 export interface NonceResponse {
-    nonce: string
-    ok: boolean
+  nonce: string
+  ok: boolean
 }
 
 export const getNonce = async (backendUrl: string) => {
-    const nonceRequest = await fetch(`${backendUrl}/auth/siwe/nonce`, {
-        method: 'GET',
-        credentials: 'include',
-      })
-      const nonceResponse = await nonceRequest.json()
+  const nonceRequest = await fetch(`${backendUrl}/auth/siwe/nonce`, {
+    method: 'GET',
+    credentials: 'include',
+  })
+  const nonceResponse = await nonceRequest.json()
 
-      return {
-          ...nonceResponse, 
-          ...nonceRequest,
-        } as NonceResponse
+  return {
+    ...nonceResponse,
+    ...nonceRequest,
+  } as NonceResponse
 }
 
 export interface AuthResponse {
-    address: string
-    ok: boolean
+  address: string
+  ok: boolean
 }
 
 export const getAuth = async (backendUrl: string) => {
-    const authRequest = await fetch(`${backendUrl}/auth/me`, {
-        method: 'GET',
-        credentials: 'include',
-      })
-      const authResponse = await authRequest.json()
+  const authRequest = await fetch(`${backendUrl}/auth/me`, {
+    method: 'GET',
+    credentials: 'include',
+  })
+  const authResponse = await authRequest.json()
 
-      return {
-          ...authResponse,
-          ...authRequest,
-      }
+  return {
+    ...authResponse,
+    ...authRequest,
+  }
 }
 
 export const postLogin = async (backendUrl: string, signature: string, message: string) => {
-    const loginRequest = await fetch(`${backendUrl}/auth/siwe/login`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          signature,
-          message,
-        }),
-      })
-      const loginResponse = await loginRequest.json()
+  const loginRequest = await fetch(`${backendUrl}/auth/siwe/login`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      signature,
+      message,
+    }),
+  })
+  const loginResponse = await loginRequest.json()
 
-      return {
-          ...loginResponse, 
-          ...loginRequest,
-        } as AuthResponse
+  return {
+    ...loginResponse,
+    ...loginRequest,
+  } as AuthResponse
 }
 
 export interface LogoutResponse {
-    ok: boolean
+  ok: boolean
 }
 
 export const postLogout = async (backendUrl: string) => {
-    const logoutRequest = await fetch(`${backendUrl}/logout/me`, {
-        method: 'POST',
-        credentials: 'include',
-      })
+  const logoutRequest = await fetch(`${backendUrl}/logout/me`, {
+    method: 'POST',
+    credentials: 'include',
+  })
 
-      return logoutRequest as LogoutResponse
+  return logoutRequest as LogoutResponse
 }
