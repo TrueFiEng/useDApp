7d3a90e10c3f6aabc79e4f2aa9d69c35793973ca
mateusz <mateusz.jandula@trusttoken.com>
2022-04-22 15:12:25 +0200

    Add mainnet balance

--- packages/docs/src/examples/Multichain.tsx
+++ packages/docs/src/examples/Multichain.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { Config, DAppProvider, ZkSyncTestnet, Arbitrum, useEtherBalance } from '@usedapp/core'
+import { Config, DAppProvider, ZkSyncTestnet, Arbitrum, useEtherBalance, Mainnet } from '@usedapp/core'
 import { formatEther } from '@ethersproject/units'
 import ReactDOM from 'react-dom'
+import { getDefaultProvider } from 'ethers'
 
 const address = '0xe13610d0a3e4303c70791773C5DF8Bb16de185d1'
 
 const config: Config = {
     readOnlyUrls: {
-      [ZkSyncTestnet.chainId]: 'https://zksync2-testnet.zksync.dev',
+      [Mainnet.chainId]: getDefaultProvider('mainnet'),
       [Arbitrum.chainId]: 'https://arb1.arbitrum.io/rpc',
+      [ZkSyncTestnet.chainId]: 'https://zksync2-testnet.zksync.dev',
     },
 }
   
@@ -20,12 +22,14 @@ ReactDOM.render(
 )
 
 export function App() {
+  const mainnetBalance = useEtherBalance(address, { chainId: Mainnet.chainId })
   const arbitrumBalance = useEtherBalance(address, { chainId: Arbitrum.chainId })
   const zkSyncBalance = useEtherBalance(address, { chainId: ZkSyncTestnet.chainId })
 
   return (
     <>
       <div> Account address: {address}</div>
+      <div> Balance on Mainnet: {mainnetBalance && formatEther(mainnetBalance)} Eth </div>
       <div> Balance on Arbitrum: {arbitrumBalance && formatEther(arbitrumBalance)} AEth </div>
       <div> Balance on ZkSync Testnet: {zkSyncBalance && formatEther(zkSyncBalance)} ZKEth </div>
     </>
