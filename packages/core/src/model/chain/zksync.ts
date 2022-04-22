b7557c3afc4d610d41a1f050b8789502e16d73d9
mateusz <mateusz.jandula@trusttoken.com>
2022-04-22 14:59:04 +0200

    Add zksync to multichain example

--- packages/core/src/model/chain/zksync.ts
+++ packages/core/src/model/chain/zksync.ts
@@ -6,6 +6,7 @@ export const ZkSyncTestnet: Chain = {
   isTestChain: true,
   isLocalChain: false,
   multicallAddress: '0x5014a961801de9a52548068bDac853CE337221e7',
+  multicall2Address: '0x32Caf123F6f574035f51532E597125062C0Aa8EE',
   getExplorerAddressLink: (address: string) => `https://zksync2-testnet.zkscan.io/address/${address}`,
   getExplorerTransactionLink: (transactionHash: string) => `https://zksync2-testnet.zkscan.io/tx/${transactionHash}`,
 }
