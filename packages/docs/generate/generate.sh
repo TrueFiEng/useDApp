ac91a2af71e069391b7b3956f614f14ee0bc14da
rzadp <roopert7@gmail.com>
2022-04-27 15:27:36 +0200

    Something is working

---
+++ packages/docs/generate/generate.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Go to root of docs package
+cd $(dirname "$0")/..
+
+jsdoc2md -c jsdoc.config.json \
+  --no-cache --no-gfm \
+  --partial generate/description.hbs \
+  --files ../core/src/**/*useCall.ts \
+  > docs/04-api.mdx
