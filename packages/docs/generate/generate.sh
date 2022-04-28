63c262ccb256a134f1268f9e44b6eb44defeb47c
rzadp <roopert7@gmail.com>
2022-04-28 12:39:46 +0200

    No index

--- packages/docs/generate/generate.sh
+++ packages/docs/generate/generate.sh
@@ -9,4 +9,5 @@ jsdoc2md -c jsdoc.config.json \
   --no-cache --no-gfm \
   --partial generate/description.hbs \
   --files '../core/src/**/hooks/**/*.{ts,tsx}' \
+  --global-index-format none \
   > "docs/03-API Reference/08-Hooks-generated.mdx"
