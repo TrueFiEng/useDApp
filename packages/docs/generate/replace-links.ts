e3a9c47abb279c8f6447e901d634552bc13b5a26
rzadp <roopert7@gmail.com>
2022-05-04 16:37:23 +0200

    More progress

--- packages/docs/generate/replace-links.ts
+++ packages/docs/generate/replace-links.ts
@@ -1,41 +1,79 @@
 import * as fs from 'fs';
 
-const filename = 'docs/03-API Reference/08-Hooks-generated.mdx'
+const filename = 'docs/03-API Reference/02-Hooks.mdx'
 
 const hooks = fs.readFileSync(filename, {encoding: 'utf-8'})
 
+const modelsLink = (value: string) => `/docs/API%20Reference/Models#${value.toLowerCase()}`
+const hooksLink = (value: string) => `/docs/API%20Reference/Models#${value.toLowerCase()}`
+
 const linkMap = {
-  'Call': '/docs/API%20Reference/Models#call'
+  'Call': modelsLink('Call'),
+  'QueryParams': modelsLink('QueryParams'),
+  'ChainCall': modelsLink('ChainCall'),
+  'RawCall': modelsLink('RawCall'),
+  'CallResult': modelsLink('CallResult'),
+  'ContractCall': modelsLink('ContractCall'),
+  'RawCallResult': modelsLink('RawCallResult'),
+  'Config': modelsLink('Config'),
+  'TransactionStatus': modelsLink('TransactionStatus'),
+  'TransactionOptions': modelsLink('TransactionOptions'),
+  'TokenInfo': modelsLink('TokenInfo'),
 }
 
+/**
+ * Can be linked to under API Reference / Models page.
+ */
+const models = [
+  'Call',
+  'QueryParams',
+  'ChainCall',
+  'RawCall',
+  'CallResult',
+  'ContractCall',
+  'RawCallResult',
+  'Config',
+  'TransactionStatus',
+  'TransactionOptions',
+  'TokenInfo'
+]
+
 const link = (value: string) => `{@link ${value}}`
-const ahref = (value: string) => `<a href="${linkMap[value]}">${value}</a>`
+const ahref = (title: string, link: string) => `<a href="${link}">${title}</a>`
 const code = (value: string) => `<code>${value}</code>`
+
+const createLink = (value: string) => {
+  if (value.startsWith('use')) {
+    return ahref(value, hooksLink(value))
+  }
+  if (models.includes(value)) return ahref(value, modelsLink(value))
+  throw new Error(`Could not find how to link to "${value}".`)
+}
 
 /**
  * Replace the {@link xxx} documentation that works in IDEs.
  * We need to point to a documentation link where the linked entity lives.
  */
 let replaced = hooks
-  .replaceAll(link('Call'), ahref('Call'))
-  .replaceAll(link('QueryParams'), code('QueryParams'))
-  .replaceAll(link('ChainCall'), code('ChainCall'))
-  .replaceAll(link('RawCall'), code('RawCall'))
-  .replaceAll(link('CallResult'), code('CallResult'))
-  .replaceAll(link('useCall'), code('useCall'))
-  .replaceAll(link('useCalls'), code('useCalls'))
-  .replaceAll(link('useRawCall'), code('useRawCall'))
-  .replaceAll(link('useRawCalls'), code('useRawCalls'))
-  .replaceAll(link('useChainCall'), code('useChainCall'))
-  .replaceAll(link('useChainCalls'), code('useChainCalls'))
-  .replaceAll(link('ContractCall'), code('ContractCall'))
-  .replaceAll(link('RawCallResult'), code('RawCallResult'))
-  .replaceAll(link('useContractFunction'), code('useContractFunction'))
-  .replaceAll(link('useSendTransaction'), code('useSendTransaction'))
-  .replaceAll(link('Config'), code('Config'))
-  .replaceAll(link('TransactionStatus'), code('TransactionStatus'))
-  .replaceAll(link('TransactionOptions'), code('TransactionOptions'))
-  .replaceAll(link('TokenInfo'), code('TokenInfo'))
+  .replaceAll(link('Call'), createLink('Call'))
+  .replaceAll(link('QueryParams'), createLink('QueryParams'))
+  .replaceAll(link('ChainCall'), createLink('ChainCall'))
+  .replaceAll(link('RawCall'), createLink('RawCall'))
+  .replaceAll(link('CallResult'), createLink('CallResult'))
+  .replaceAll(link('useCall'), createLink('useCall'))
+  .replaceAll(link('useCalls'), createLink('useCalls'))
+  .replaceAll(link('useRawCall'), createLink('useRawCall'))
+  .replaceAll(link('useRawCalls'), createLink('useRawCalls'))
+  .replaceAll(link('useChainCall'), createLink('useChainCall'))
+  .replaceAll(link('useChainCalls'), createLink('useChainCalls'))
+  .replaceAll(link('ContractCall'), createLink('ContractCall'))
+  .replaceAll(link('RawCallResult'), createLink('RawCallResult'))
+  .replaceAll(link('useContractFunction'), createLink('useContractFunction'))
+  .replaceAll(link('useSendTransaction'), createLink('useSendTransaction'))
+  .replaceAll(link('Config'), createLink('Config'))
+  .replaceAll(link('TransactionStatus'), createLink('TransactionStatus'))
+  .replaceAll(link('TransactionOptions'), createLink('TransactionOptions'))
+  .replaceAll(link('TokenInfo'), createLink('TokenInfo'))
 
 /**
  * Those paragraphs are generated all over the place but they only cause trouble.
