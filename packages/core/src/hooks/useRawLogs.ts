c1bc31b4dfd213798df389dfa7049505ac71772b
Tyler Loewen <tyler@trilez.com>
2022-05-05 17:15:23 -0700

    :wood: Add useRawLogs hook

---
+++ packages/core/src/hooks/useRawLogs.ts
@@ -0,0 +1,39 @@
+import { useBlockNumber } from '../providers/blockNumber/blockNumber/context'
+import { useEffect, useMemo, useState } from 'react'
+import { useEthers } from './useEthers'
+import { useReadonlyNetworks } from '../providers/network/readonlyNetworks'
+import { useBlockNumbers } from '../providers/blockNumber/blockNumbers'
+import { QueryParams } from '../constants/type/QueryParams'
+import { Filter, FilterByBlockHash, Log } from '@ethersproject/abstract-provider'
+
+/**
+ * @public
+ */
+export function useRawLogs(
+  filter: Filter | FilterByBlockHash | Promise<Filter | FilterByBlockHash>,
+  queryParams: QueryParams = {}
+): Log[] | undefined {
+  const { library } = useEthers()
+  const providers = useReadonlyNetworks()
+  const _blockNumber = useBlockNumber()
+  const blockNumbers = useBlockNumbers()
+
+  const [logs, setLogs] = useState<Log[] | undefined>()
+
+  const { chainId } = queryParams
+
+  const [provider, blockNumber] = useMemo(
+    () => (chainId ? [providers[chainId], blockNumbers[chainId]] : [library, _blockNumber]),
+    [providers, library, blockNumbers, _blockNumber]
+  )
+
+  async function updateLogs() {
+    setLogs(await provider?.getLogs(filter))
+  }
+
+  useEffect(() => {
+    void updateLogs()
+  }, [provider, blockNumber])
+
+  return logs
+}
