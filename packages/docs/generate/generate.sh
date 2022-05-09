f7b735e0e1b3526072b4b826d32a11dedee41287
Przemyslaw Rzad <przemyslaw.rzad@trusttoken.com>
2022-05-09 16:02:52 +0200

    Update packages/docs/generate/generate.sh
    Co-authored-by: Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>

--- packages/docs/generate/generate.sh
+++ packages/docs/generate/generate.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 # Go to root of docs package
 cd $(dirname "$0")/..
 
-ts-node -r ts-node/register/transpile-only \
+ts-node \
   --project ./tsconfig.node.json \
   ./generate/generate-content.ts
