bbcad076fbd972d6c01b6a01c6cc50522f3f138d
rzadp <roopert7@gmail.com>
2022-05-09 12:27:14 +0200

    Remove obsolete comment

--- packages/docs/generate/generate.sh
+++ packages/docs/generate/generate.sh
@@ -4,8 +4,6 @@ set -euo pipefail
 # Go to root of docs package
 cd $(dirname "$0")/..
 
-# pnpx typedoc --json ./generate/hooks.gen.json --tsconfig '../core/tsconfig.json' '../core/src/hooks'
-
 ts-node -r ts-node/register/transpile-only \
   --project ./tsconfig.node.json \
   ./generate/generate-content.ts
