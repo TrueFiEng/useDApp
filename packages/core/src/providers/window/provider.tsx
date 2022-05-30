797520e9a33428114b3a1288f7317ed921db3c27
mateusz <mateusz.jandula@trusttoken.com>
2022-05-30 16:31:17 +0200

    Add window provider

---
+++ packages/core/src/providers/window/provider.tsx
@@ -0,0 +1,26 @@
+import { ReactNode, useState } from 'react'
+import { WindowContext } from './context'
+
+interface Props {
+  children: ReactNode
+}
+
+/**
+ * @internal Intended for internal use - use it on your own risk
+ */
+export function WindowProvider({ children }: Props) {
+  const [isActiveWindow, setActiveWindow] = useState(true)
+
+  document.addEventListener('visibilitychange', () => {
+    switch (document.visibilityState) {
+      case 'hidden':
+        setActiveWindow(false)
+        break
+      case 'visible':
+        setActiveWindow(true)
+        break
+    }
+  })
+
+  return <WindowContext.Provider value={{isActive: isActiveWindow}} children={children} />
+}
