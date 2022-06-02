b6b99d7379e5e5f96aa7551934afb13a3417bec7
mateusz <mateusz.jandula@trusttoken.com>
2022-06-02 10:54:56 +0200

    Add refreshing tests

--- packages/core/src/hooks/refreshing.test.ts
+++ packages/core/src/hooks/refreshing.test.ts
@@ -1,49 +1,149 @@
 import { expect } from 'chai'
-import { BigNumber, Wallet } from 'ethers'
-import { Config } from '../constants'
+import { Wallet } from 'ethers'
+import { Config, Refresh } from '../constants'
 import { TestingNetwork, renderDAppHook, setupTestingConfig } from '../testing'
 import { useEtherBalance } from './useEtherBalance'
+import { sleep } from '../testing/utils/waitUntil'
 
 
-describe.skip('useEtherBalance', () => {
+describe('useEtherBalance with refreshing', () => {
     let network1: TestingNetwork
-    let network2: TestingNetwork
     let config: Config
     const receiver = Wallet.createRandom().address
+
     const mineBlock = async () => {
         await network1.wallets[0].sendTransaction({ to: receiver, value: 100 })
     }
 
     beforeEach(async () => {
-        ;({ config, network1, network2 } = await setupTestingConfig())
+        ;({ config, network1 } = await setupTestingConfig())
         await network1.wallets[0].sendTransaction({ to: receiver, value: 100 })
-        await network2.wallets[1].sendTransaction({ to: receiver, value: 200 })
     })
 
-    it('do not change static value when changing ether value', async () => {
-        const { result, waitForCurrent, waitForNextUpdate } = await renderDAppHook(() => useEtherBalance(receiver), {
-        config,
+    it('does not change value with never refreshing with global config', async () => {
+        const { result, waitForCurrent } = await renderDAppHook(() => useEtherBalance(receiver), {
+            config: {
+                ...config,
+                refresh: Refresh.Never,
+            },
         })
-        // await waitForCurrent((val) => val !== undefined)
-        // expect(result.error).to.be.undefined
-        // expect(result.current).to.eq(100)
-        await mineBlock()
-        // expect(result.error).to.be.undefined
-        // expect(result.current).to.eq(100)
-        await mineBlock()
-        // expect(result.error).to.be.undefined
-        // expect(result.current).to.eq(100)
-        await mineBlock()
-        // await waitForNextUpdate()
-        await mineBlock()
-        await mineBlock()
-        await mineBlock()
-        await waitForCurrent((val) => {
-            console.log(val?.toString())
-            return false
-        })
-
+        await waitForCurrent((val) => val !== undefined)
         expect(result.error).to.be.undefined
-        expect(result.current).to.eq(400)
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        await waitForCurrent((val) => val !== undefined)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        await waitForCurrent((val) => val !== undefined)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+    })
+
+    it('does not change value with never refreshing with query param', async () => {
+        const { result, waitForCurrent } = await renderDAppHook(() => useEtherBalance(receiver, {
+            refreshPerBlocks: Refresh.Never,
+        }), {
+            config
+        })
+        await waitForCurrent((val) => val !== undefined)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        await waitForCurrent((val) => val !== undefined)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        await waitForCurrent((val) => val !== undefined)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+    })
+
+    it('does change value every block', async () => {
+        const { result, waitForCurrent } = await renderDAppHook(() => useEtherBalance(receiver), {
+            config: {
+                ...config,
+                refresh: Refresh.EveryBlock,
+            },
+        })
+        await waitForCurrent((val) => val !== undefined)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        await waitForCurrent((val) => val?.toString() === '200')
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(200)
+    })
+
+    it('overrides global config with query param', async () => {
+        const { result, waitForCurrent } = await renderDAppHook(() => useEtherBalance(receiver, {
+            refreshPerBlocks: Refresh.EveryBlock,
+        }), {
+            config
+        })
+        await waitForCurrent((val) => val !== undefined)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        await waitForCurrent((val) => val?.toString() === '200')
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(200)
+    })
+
+    it('does change value after specified number of blocks with global config', async () => {
+        const { result, waitForCurrent } = await renderDAppHook(() => useEtherBalance(receiver), {
+            config: {
+                ...config,
+                refresh: 2,
+            },
+        })
+        await waitForCurrent((val) => val !== undefined)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        await waitForCurrent((val) => val?.toString() === '300')
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(300)
+    })
+
+    it('does change value after specified number of blocks with query param', async () => {
+        const { result, waitForCurrent } = await renderDAppHook(() => useEtherBalance(receiver, {
+            refreshPerBlocks: 2
+        }), {
+            config,
+        })
+        await waitForCurrent((val) => val !== undefined)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(100)
+        await sleep(1000)
+        await mineBlock()
+        await sleep(1000)
+        await waitForCurrent((val) => val?.toString() === '300')
+        expect(result.error).to.be.undefined
+        expect(result.current).to.eq(300)
     })
 })
