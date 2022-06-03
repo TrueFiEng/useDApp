60ee8f7932d96166bfa00df0eb93499eba64d538
yivlad <jacenko.vlad@gmail.com>
2022-06-03 14:47:37 +0200

    Fix tests

--- packages/core/src/providers/network/readonlyNetworks/readonlyNetworksProvider.test.ts
+++ packages/core/src/providers/network/readonlyNetworks/readonlyNetworksProvider.test.ts
@@ -1,7 +1,10 @@
 import { expect } from 'chai'
+import { providers } from 'ethers'
 import { getProvidersFromConfig } from './provider'
 import { Kovan, Mainnet, Rinkeby, Localhost } from '../../../model/chain'
 import { MockProvider } from 'ethereum-waffle'
+
+const JsonRpcProvider = providers.JsonRpcProvider;
 
 describe('ReadonlyNetworksProvider', () => {
   it('getProvidersFromConfig creates provider for each network that has URL', async () => {
@@ -16,7 +19,7 @@ describe('ReadonlyNetworksProvider', () => {
       Rinkeby.chainId.toString(),
       Kovan.chainId.toString(),
     ])
-    expect(providers[Mainnet.chainId]).to.be.instanceOf(providers.JsonRpcProvider)
+    expect(providers[Mainnet.chainId]).to.be.instanceOf(JsonRpcProvider)
   })
 
   it('getProvidersFromConfig fetches provider object', async () => {
