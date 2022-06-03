3c6ab7d7d3e9f73c2f1ca8134c7670abca33a56e
rzadp <roopert7@gmail.com>
2022-06-03 15:28:01 +0200

    Add more info

--- packages/core/src/hooks/useLogs.ts
+++ packages/core/src/hooks/useLogs.ts
@@ -20,7 +20,7 @@ export interface TypedFilter<
 /**
  * Makes a call to get the logs for a specific contract event and returns the decoded logs or an error if present.
  * The hook will cause the component to refresh when a new block is mined and the returned logs change.
- * A syntax sugar for useRawLogs that uses ABI, event name, and arguments instead of raw data.
+ * A syntax sugar for {@link useRawLogs} that uses ABI, event name, and arguments instead of raw data.
  * @param filter an event filter (see {@link TypedFilter})
  * @param queryParams allows for additional configuration of the query (see {@link LogQueryParams})
  * @returns an array of decoded logs (see {@link LogsResult})
--- packages/core/src/hooks/useRawLogs.ts
+++ packages/core/src/hooks/useRawLogs.ts
@@ -8,6 +8,11 @@ import { Falsy } from '../model/types'
 
 /**
  * Returns all blockchain logs given a block filter.
+ * The hook will cause the component to refresh when a new block is mined and the returned logs change.
+ * @see {@link useLogs} for a more easy-to-use version of the query.
+ * @param filter an event filter, which blocks to query
+ * @param queryParams allows for additional configuration of the query (see {@link QueryParams})
+ * @returns an array of [logs](https://docs.ethers.io/v5/api/providers/types/#providers-Log)
  * @public
  */
 export function useRawLogs(
