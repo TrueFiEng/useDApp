8c8ee0a5f4ad9a4924826849e09ce383be6b3bd1
mj426382 <mateusz.jandula@trusttoken.com>
2022-06-09 16:27:58 +0200

    Remove old test without new config

--- packages/core/src/hooks/useBlockMeta.test.tsx
+++ packages/core/src/hooks/useBlockMeta.test.tsx
@@ -21,20 +21,7 @@ describe('useBlockMeta', () => {
     expect(result.current.difficulty).to.not.be.undefined
   })
 
-  it('updates the block timestamp when a transaction gets mined', async () => {
-    const { result, mineBlock, waitForCurrent } = await renderWeb3Hook(useBlockMeta)
-    await waitForCurrent((val) => val.timestamp !== undefined && val.difficulty !== undefined)
-
-    expect(result.error).to.be.undefined
-    const firstTimestamp = result.current.timestamp
-
-    await sleep(1000)
-    await mineBlock()
-    await waitForCurrent((val) => val.timestamp?.getTime() !== firstTimestamp?.getTime())
-    expect(result.current.timestamp).to.be.greaterThan(firstTimestamp)
-  })
-
-  it('updates the block timestamp when a transaction gets mined qith dappHook', async () => {
+  it('updates the block timestamp when a transaction gets mined with dappHook', async () => {
     const { result, waitForCurrent } = await renderDAppHook(useBlockMeta, { config })
     await waitForCurrent((val) => val.timestamp !== undefined && val.difficulty !== undefined)
 
