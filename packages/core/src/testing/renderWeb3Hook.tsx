ba13b70eac5a19821e27c00f3fff5488e5ac2234
yivlad <jacenko.vlad@gmail.com>
2022-06-21 11:17:36 +0200

    Lint

--- packages/core/src/testing/renderWeb3Hook.tsx
+++ packages/core/src/testing/renderWeb3Hook.tsx
@@ -5,7 +5,6 @@ import React from 'react'
 import { deployMulticall, getWaitUtils, IdentityWrapper, mineBlock } from './utils'
 import { BlockNumbersProvider } from '../providers/blockNumber/blockNumbers'
 import { ReadonlyNetworksProvider } from '../providers/network'
-import { providers as ethersProviders } from 'ethers'
 
 export interface renderWeb3HookOptions<Tprops> {
   mockProvider?: MockProvider
