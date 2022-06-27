520287bc39ac964e7bf9ba0ebe33ed6ecb359583
mj426382 <mateusz.jandula@trusttoken.com>
2022-06-27 14:32:59 +0200

    Fix lint

--- packages/core/src/providers/network/network/provider.tsx
+++ packages/core/src/providers/network/network/provider.tsx
@@ -1,9 +1,8 @@
 import { ReactNode, useCallback, useContext, useEffect, useReducer, useState } from 'react'
 import { NetworkContext } from './context'
 import { defaultNetworkState, networkReducer } from './reducer'
-import { Network } from './model'
 import { providers } from 'ethers'
-import { subscribeToProviderEvents, getInjectedProvider } from '../../../helpers'
+import { subscribeToProviderEvents } from '../../../helpers'
 import { useLocalStorage, useConfig } from '../../../hooks'
 import detectEthereumProvider from '@metamask/detect-provider'
 import { ConnectorContext, MetamaskConnector } from '../connector'
@@ -12,8 +11,6 @@ import { ConnectorController } from '../connector/connectorController'
 
 type JsonRpcProvider = providers.JsonRpcProvider
 type ExternalProvider = providers.ExternalProvider
-const Provider = providers.Provider
-const Web3Provider = providers.Web3Provider
 
 interface NetworkProviderProps {
   children: ReactNode
@@ -24,7 +21,7 @@ interface NetworkProviderProps {
  * @internal Intended for internal use - use it on your own risk
  */
 export function NetworkProvider({ children, providerOverride }: NetworkProviderProps) {
-  const { autoConnect, pollingInterval, noMetamaskDeactivate, pollingIntervals } = useConfig()
+  const { autoConnect } = useConfig()
 
   const [network, dispatch] = useReducer(networkReducer, defaultNetworkState)
   const [onUnsubscribe, setOnUnsubscribe] = useState<() => void>(() => () => undefined)
