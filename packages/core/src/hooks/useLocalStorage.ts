79466171696d093c2f6bad25916bb073dce23e2b
mj426382 <mateusz.jandula@trusttoken.com>
2022-06-28 11:14:50 +0200

    Add local storage improvement

--- packages/core/src/hooks/useLocalStorage.ts
+++ packages/core/src/hooks/useLocalStorage.ts
@@ -1,27 +1,50 @@
 import { useEffect, useState } from 'react'
 import { useConfig } from './useConfig'
 
+function getItem(key: string, localStorage: WindowLocalStorage['localStorage']) {
+  if (typeof window === 'undefined') {
+    return null
+  }
+
+  const item = localStorage.getItem(key)
+  if (item !== null) {
+    try {
+      return JSON.parse(item)
+    } catch {
+      // ignore error
+    }
+  }
+}
+
+function setItem(key: string, value: any, localStorage: WindowLocalStorage['localStorage']) {
+  if (value === undefined) {
+    localStorage.removeItem(key)
+  } else {
+    const toStore = JSON.stringify(value)
+    localStorage.setItem(key, toStore)
+    return JSON.parse(toStore)
+  }
+}
+
 /**
  * @internal Intended for internal use - use it on your own risk
  */
 export function useLocalStorage(key: string) {
-  const {
-    localStorageOverride: { getItem, setItem },
-  } = useConfig()
-  const [value, setValue] = useState<any>(() => getItem(key))
+  const { localStorageOverride } = useConfig()
+  const [value, setValue] = useState(() => getItem(key, localStorageOverride))
 
   useEffect(() => {
-    setValue(getItem(key))
+    setValue(getItem(key, localStorageOverride))
   }, [key])
 
   useEffect(() => {
     if (!value) {
       return
     }
-    setItem(key, value)
+    setItem(key, value, localStorageOverride)
   }, [value])
 
   // As value updating relies on useEffect, it takes mutliple rerenders to fully update the value.
   // The third elemnt in the return array allows to get the immediate value stored in the localStorage.
-  return [value, setValue, () => getItem(key)] as const
+  return [value, setValue, () => getItem(key, localStorageOverride)] as const
 }
