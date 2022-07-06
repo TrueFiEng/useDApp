e818b80b95287cc95c8bd4a9bfc60c317d2b0221
yivlad <jacenko.vlad@gmail.com>
2022-07-06 12:20:58 +0200

    Add refreshing test when refresh set to never

--- packages/core/src/hooks/useCall.test.tsx
+++ packages/core/src/hooks/useCall.test.tsx
@@ -9,11 +9,13 @@ import {
   SECOND_TEST_CHAIN_ID,
   SECOND_MOCK_TOKEN_INITIAL_BALANCE,
   getResultPropery,
+  renderDAppHook,
+  setupTestingConfig,
 } from '../testing'
 import { ChainId } from '../constants/chainId'
 import { BigNumber } from 'ethers'
 import { deployContract } from 'ethereum-waffle'
-import { BlockNumberContract, RevertContract } from '../constants'
+import { BlockNumberContract, RevertContract, doublerContractABI } from '../constants'
 import waitForExpect from 'wait-for-expect'
 
 describe('useCall', () => {
@@ -217,6 +219,34 @@ describe('useCall', () => {
           // we don't actually know when the update is gonna happen - both possibilities are possible
           expect(block2 === blockNumber + 4 || block2 === blockNumber + 5).to.be.true
         })
+      })
+
+      it('Refreshes static call on parameter change', async () => {
+        const { config, network1 } = await setupTestingConfig();
+        const [deployer] = network1.provider.getWallets()
+        const doublerContract = await deployContract(deployer, doublerContractABI)
+        const { waitForCurrent, rerender } = await renderDAppHook(
+          ({ num }: { num: number }) => useCall({
+            contract: doublerContract,
+            method: 'double',
+            args: [num]
+          }), {
+          config: {
+            ...config,
+            refresh: 'never'
+          },
+          renderHook: {
+            initialProps: {
+              num: 1
+            }
+          }
+        })
+
+        await waitForCurrent(val => val?.value?.[0]?.eq(2))
+
+        rerender({ num: 2 })
+
+        await waitForCurrent(val => val?.value?.[0]?.eq(4))
       })
     })
   }
