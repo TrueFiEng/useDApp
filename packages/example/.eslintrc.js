3dbf9ed57359d497a0a7347647b392e82846e743
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2022-07-19 15:04:52 +0200

    Linting

--- packages/example/.eslintrc.js
+++ packages/example/.eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
   extends: [`${__dirname}/../../.eslintrc.json`],
   parserOptions: {
-    "project": "./tsconfig.test.json",
-    "sourceType": "module"
+    sourceType: 'module',
   },
 }
