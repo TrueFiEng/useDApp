786077fae780933f10dc9224f55eef75088e0983
Vladyslav Mazurenko <mazur097mazur@gmail.com>
2022-07-20 16:38:10 +0200

    Style fixed header

---
+++ packages/website/src/js/main.js
@@ -0,0 +1,15 @@
+const body = document.body
+const header = body.querySelector('header')
+
+const headerOffset = header.clientHeight
+
+const handleHeaderStyles = () => {
+  if (window.scrollY < headerOffset) {
+    header.classList.remove('header--fixed')
+  }
+  if (window.scrollY >= headerOffset) {
+    header.classList.add('header--fixed')
+  }
+}
+
+document.addEventListener('scroll', handleHeaderStyles)
