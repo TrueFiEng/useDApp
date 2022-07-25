f9ab7109d6c48842a63dfda018a682a3fb4489ad
Vladyslav Mazurenko <mazur097mazur@gmail.com>
2022-07-25 12:13:40 +0200

    Animate hero section paths

--- packages/website/src/js/main.js
+++ packages/website/src/js/main.js
@@ -1,4 +1,4 @@
-// animations
+// animations on scroll
 AOS.init()
 
 // header
@@ -17,6 +17,26 @@ const handleHeaderStyles = () => {
 }
 
 document.addEventListener("scroll", handleHeaderStyles)
+
+// motion path
+
+const ethereumIllustration = body.querySelector('.hero__ethereum')
+const reactIllustration = body.querySelector('.hero__react')
+
+ethereumIllustration.querySelectorAll('.hero__ethereum > *').forEach(
+  (item, index) => {
+    item.style.setProperty('stroke-dasharray', (item.getTotalLength() + 1).toString())
+    item.style.setProperty('stroke-dashoffset', (item.getTotalLength() + 1).toString())
+    item.style.setProperty('animation-delay', `${10 * index / 100}s`)
+  }
+)
+reactIllustration.querySelectorAll('.hero__react > *').forEach(
+  (item, index) => {
+    item.style.setProperty('stroke-dasharray', (item.getTotalLength() + 1).toString())
+    item.style.setProperty('stroke-dashoffset', (item.getTotalLength() + 1).toString())
+    item.style.setProperty('animation-delay', `${20 * index / 100}s`)
+  }
+)
 
 // marquee
 Marquee3k.init()
