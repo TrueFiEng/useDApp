2f1d594dcac6158d965fb8984d7c166d811e3d6f
yivlad <jacenko.vlad@gmail.com>
2022-07-28 16:50:38 +0200

    Lint

--- packages/playwright/src/pageDiagnostics.ts
+++ packages/playwright/src/pageDiagnostics.ts
@@ -18,7 +18,7 @@ export const addPageDiagnostics = (page: Page) => {
     if (ignoredLogs.some((log) => msg.text()?.includes(log))) return
     console.log(msg.text()) // Logs shown in the browser, will be retransmitted in Node logs as well.
   })
-  page.on('pageerror', (e) => {
+  page.on('pageerror', (/* e */) => {
     // Errors in the browser will error out the playwright tests.
     // throw new Error(`Unhandled exception in the page: ${e}`)
   })
