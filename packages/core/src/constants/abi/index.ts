3c0128e6491b3de784e7540e1cb0b96ec673e9c0
rzadp <roopert7@gmail.com>
2022-08-04 11:18:01 +0200

    🧂 Add some resilency tests

--- packages/core/src/constants/abi/index.ts
+++ packages/core/src/constants/abi/index.ts
@@ -4,7 +4,6 @@ import MultiCall2 from './MultiCall2.json'
 import ERC20 from './ERC20.json'
 import ERC20Mock from './ERC20Mock.json'
 import BlockNumberContract from './BlockNumber.json'
-import RevertContract from './Revert.json'
 
 const Interface = utils.Interface
 
@@ -26,7 +25,6 @@ export { ERC20Mock, ERC20MockInterface }
 
 export { BlockNumberContract }
 
-export { RevertContract }
-
 export * from './doubler'
 export * from './timestamp'
+export * from './reverter'
