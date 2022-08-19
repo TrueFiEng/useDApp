aebef5936c0686e4122ac230057fb15a7ea8efb6
rzadp <roopert7@gmail.com>
2022-08-19 16:08:38 +0200

    Exit cleanly everywhere

--- packages/example/test/test-setup.ts
+++ packages/example/test/test-setup.ts
@@ -1,7 +1,12 @@
-import 'jsdom-global/register'
 import chai from 'chai'
 import { solidity } from 'ethereum-waffle'
 import chaiAsPromised from 'chai-as-promised'
 
 chai.use(solidity)
 chai.use(chaiAsPromised)
+
+let jsdomCleanup: any
+before(() => {
+  jsdomCleanup = require('jsdom-global')(undefined, {url: 'http://localhost/'})
+})
+after(() => jsdomCleanup?.())
