5c75f939ede4ae071e7eaaf7881bfc782268cb3a
yivlad <jacenko.vlad@gmail.com>
2022-08-24 17:00:30 +0200

    Clean up next tests

--- packages/example-next/playwright/test-setup.ts
+++
@@ -1,7 +0,0 @@
-import 'jsdom-global/register'
-import chai from 'chai'
-import { solidity } from 'ethereum-waffle'
-import chaiAsPromised from 'chai-as-promised'
-
-chai.use(solidity)
-chai.use(chaiAsPromised)
