5a429823406edcfddc3fbbd190800a4c8ad03ac4
rzadp <roopert7@gmail.com>
2022-09-06 14:32:36 +0200

    Fix for: Uncaught DOMException: Failed to execute 'querySelector' on 'Document': The provided selector is empty.

--- packages/docs/static/mdx-anchor-scroll.js
+++ packages/docs/static/mdx-anchor-scroll.js
@@ -1,4 +1,5 @@
 window.addEventListener('load', () => {
+  if (!window.location.hash) return
   const destination = document.querySelector(window.location.hash)
   if (destination) {
     destination.scrollIntoView()
