11b09ef4e5a8f1bb46caacfc6cb96922f2f0622e
rzadp <roopert7@gmail.com>
2022-09-12 14:28:48 +0200

    Hack for jsdom cleanup

--- packages/uniswap/src/test-setup.ts
+++ packages/uniswap/src/test-setup.ts
@@ -1,7 +1,13 @@
-import 'jsdom-global/register'
 import chai from 'chai'
 import { solidity } from 'ethereum-waffle'
 import chaiAsPromised from 'chai-as-promised'
+
+let jsdomCleanup: any
+before(() => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  jsdomCleanup = require('jsdom-global')(undefined, { url: 'http://localhost/' })
+})
+after(() => jsdomCleanup?.())
 
 chai.use(solidity)
 chai.use(chaiAsPromised)
