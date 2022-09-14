f404b5a0aa570d234edbb8cb3b0929432df5ccb8
rzadp <roopert7@gmail.com>
2022-09-14 14:13:14 +0200

    Lint

--- packages/docs/src/examples/ContractFunction.tsx
+++ packages/docs/src/examples/ContractFunction.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { DAppProvider, useEthers, useContractFunction, Config, Goerli, Kovan, Rinkeby, Ropsten } from '@usedapp/core'
 import { getDefaultProvider, utils } from 'ethers'
@@ -23,35 +23,35 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
+const WrapEtherComponent = (props: { chainId: number }) => {
+  const wethAddress = WETH_ADDRESSES[props.chainId]
+  const wethInterface = new utils.Interface(WethAbi)
+  const contract = new Contract(wethAddress, wethInterface) as any
+
+  const { state, send } = useContractFunction(contract, 'deposit', {
+    transactionName: 'Wrap',
+    gasLimitBufferPercentage: 10,
+  })
+  const { status } = state
+
+  const wrapEther = () => {
+    void send({ value: 1 })
+  }
+
+  return (
+    <div>
+      <button onClick={() => wrapEther()}>Wrap ether</button>
+      <p>Status: {status}</p>
+    </div>
+  )
+}
+
 export function App() {
   const { account, chainId } = useEthers()
-
-  const WrapEtherComponent = useCallback(() => {
-    const wethAddress = WETH_ADDRESSES[chainId]
-    const wethInterface = new utils.Interface(WethAbi)
-    const contract = new Contract(wethAddress, wethInterface) as any
-
-    const { state, send } = useContractFunction(contract, 'deposit', {
-      transactionName: 'Wrap',
-      gasLimitBufferPercentage: 10,
-    })
-    const { status } = state
-
-    const wrapEther = () => {
-      void send({ value: 1 })
-    }
-
-    return (
-      <div>
-        <button onClick={() => wrapEther()}>Wrap ether</button>
-        <p>Status: {status}</p>
-      </div>
-    )
-  }, [chainId])
 
   if (!config.readOnlyUrls[chainId]) {
     return <p>Please use either Goerli, Kovan, Rinkeby or Ropsten testnet.</p>
   }
 
-  return <div>{!account ? <MetamaskConnect /> : <WrapEtherComponent />}</div>
+  return <div>{!account ? <MetamaskConnect /> : <WrapEtherComponent chainId={chainId} />}</div>
 }
