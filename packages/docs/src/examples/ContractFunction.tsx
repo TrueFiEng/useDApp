5332283efb3ceaf9c95ccdb06ebd85a11de0198d
Przemyslaw Rzad <przemyslaw.rzad@trusttoken.com>
2022-09-14 15:02:37 +0200

    🌵 Remove unnecessary re-renders that lose transaction state in the docs (#972)

--- packages/docs/src/examples/ContractFunction.tsx
+++ packages/docs/src/examples/ContractFunction.tsx
@@ -23,34 +23,35 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
+const WrapEtherComponent = (props: { chainId: number }) => {
+  const wethAddress = WETH_ADDRESSES[props.chainId]
+  const wethInterface = new utils.Interface(WethAbi)
+  const contract = new Contract(wethAddress, wethInterface) as any
+
+  const { state, send } = useContractFunction(contract, 'deposit', {
+    transactionName: 'Wrap',
+    gasLimitBufferPercentage: 10,
+  })
+  const { status } = state
+
+  const wrapEther = () => {
+    void send({ value: 1 })
+  }
+
+  return (
+    <div>
+      <button onClick={() => wrapEther()}>Wrap ether</button>
+      <p>Status: {status}</p>
+    </div>
+  )
+}
+
 export function App() {
   const { account, chainId } = useEthers()
+
   if (!config.readOnlyUrls[chainId]) {
     return <p>Please use either Goerli, Kovan, Rinkeby or Ropsten testnet.</p>
   }
 
-  const WrapEtherComponent = () => {
-    const wethAddress = WETH_ADDRESSES[chainId]
-    const wethInterface = new utils.Interface(WethAbi)
-    const contract = new Contract(wethAddress, wethInterface) as any
-
-    const { state, send } = useContractFunction(contract, 'deposit', {
-      transactionName: 'Wrap',
-      gasLimitBufferPercentage: 10,
-    })
-    const { status } = state
-
-    const wrapEther = () => {
-      void send({ value: 1 })
-    }
-
-    return (
-      <div>
-        <button onClick={() => wrapEther()}>Wrap ether</button>
-        <p>Status: {status}</p>
-      </div>
-    )
-  }
-
-  return <div>{!account ? <MetamaskConnect /> : <WrapEtherComponent />}</div>
+  return <div>{!account ? <MetamaskConnect /> : <WrapEtherComponent chainId={chainId} />}</div>
 }
