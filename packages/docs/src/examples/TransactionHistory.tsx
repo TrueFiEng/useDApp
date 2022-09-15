a2ed5a98be302d21b8e4d85b29c90c6937c51739
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2022-09-15 09:52:25 +0200

    Remove unnecessary re-renders that lose transaction state in the docs

--- packages/docs/src/examples/TransactionHistory.tsx
+++ packages/docs/src/examples/TransactionHistory.tsx
@@ -32,49 +32,49 @@ ReactDOM.render(
   </DAppProvider>,
   document.getElementById('root')
 )
+const WrapEtherComponent = () => {
+  const { transactions } = useTransactions()
+  const { chainId } = useEthers()
+  const wethAddress = WETH_ADDRESSES[chainId]
+  const wethInterface = new utils.Interface(WethAbi)
+  const contract = new Contract(wethAddress, wethInterface) as any
+
+  const { state, send } = useContractFunction(contract, 'deposit', { transactionName: 'Wrap' })
+  const { status } = state
+
+  const wrapEther = () => {
+    void send({ value: 1 })
+  }
+
+  return (
+    <div>
+      <button onClick={() => wrapEther()}>Wrap ether</button>
+      <p>Status: {status}</p>
+      <p>Transactions</p>
+      {transactions.length !== 0 && (
+        <table>
+          <th>Name</th>
+          <th>Block hash</th>
+          <th>Date</th>
+          {transactions.map((transaction) => {
+            return (
+              <tr>
+                <td>{transaction.transactionName}</td>
+                <td>{transaction.receipt?.blockHash ?? 'Pending...'}</td>
+                <td>{new Date(transaction.submittedAt).toDateString()}</td>
+              </tr>
+            )
+          })}
+        </table>
+      )}
+    </div>
+  )
+}
 
 export function App() {
-  const { transactions } = useTransactions()
   const { account, chainId } = useEthers()
   if (!config.readOnlyUrls[chainId]) {
     return <p>Please use either Goerli, Kovan, Rinkeby or Ropsten testnet.</p>
-  }
-
-  const WrapEtherComponent = () => {
-    const wethAddress = WETH_ADDRESSES[chainId]
-    const wethInterface = new utils.Interface(WethAbi)
-    const contract = new Contract(wethAddress, wethInterface) as any
-
-    const { state, send } = useContractFunction(contract, 'deposit', { transactionName: 'Wrap' })
-    const { status } = state
-
-    const wrapEther = () => {
-      void send({ value: 1 })
-    }
-
-    return (
-      <div>
-        <button onClick={() => wrapEther()}>Wrap ether</button>
-        <p>Status: {status}</p>
-        <p>Transactions</p>
-        {transactions.length !== 0 && (
-          <table>
-            <th>Name</th>
-            <th>Block hash</th>
-            <th>Date</th>
-            {transactions.map((transaction) => {
-              return (
-                <tr>
-                  <td>{transaction.transactionName}</td>
-                  <td>{transaction.receipt?.blockHash ?? 'Pending...'}</td>
-                  <td>{new Date(transaction.submittedAt).toDateString()}</td>
-                </tr>
-              )
-            })}
-          </table>
-        )}
-      </div>
-    )
   }
 
   return <div>{!account ? <MetamaskConnect /> : <WrapEtherComponent />}</div>
