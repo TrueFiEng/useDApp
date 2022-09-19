591e0585ac8432b8165bf14289de8f42ea72fb7c
nezouse <stankiewicz1998@gmail.com>
2022-09-19 23:48:32 +0200

    🧑🏻‍🚀 Remove unnecessary useMemo in useTransactions

--- packages/core/src/hooks/useTransactions.ts
+++ packages/core/src/hooks/useTransactions.ts
@@ -28,9 +28,7 @@ import { useEthers } from './useEthers'
 export function useTransactions(queryParams: QueryParams = {}) {
   const { chainId: defaultChainId, account } = useEthers()
   const { addTransaction, transactions } = useTransactionsContext()
-  const { chainId: _chainId } = queryParams
-
-  const chainId = useMemo(() => _chainId ?? defaultChainId, [_chainId, defaultChainId])
+  const { chainId = defaultChainId } = queryParams
 
   const filtered = useMemo(() => {
     if (chainId === undefined || !account) {
