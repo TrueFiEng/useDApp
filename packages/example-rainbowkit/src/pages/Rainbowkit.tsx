fbcec6adb51b798d1f48e0493ce08783e95e93e7
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2022-09-20 15:01:12 +0200

    Add modal buttons

--- packages/example-rainbowkit/src/pages/Rainbowkit.tsx
+++ packages/example-rainbowkit/src/pages/Rainbowkit.tsx
@@ -1,38 +1,8 @@
 import React from 'react'
 import { Container, MainContent } from '../components/base/base'
 import { ConnectButton, useAccountModal, useChainModal, useConnectModal } from '@usedapp/rainbowkit'
-import styled from 'styled-components'
 import { Button } from '../components/base/Button';
 
-// const Button = styled.button`
-//   z-index: 100;
-//   display: grid;
-//   grid-auto-flow: column;
-//   grid-column-gap: 8px;
-//   align-items: center;
-//   width: fit-content;
-//   min-width: 160px;
-//   height: 40px;
-//   font-family: 'Roboto', sans-serif;
-//   font-size: 14px;
-//   line-height: 24px;
-//   font-weight: 700;
-//   text-transform: uppercase;
-//   letter-spacing: 0.1em;
-//   color: white;
-//   padding: 5px;
-//   border: 1px solid #ffffff;
-//   border-radius: 14px;
-//   background-color: blue;
-// cursor: pointer;
-//   // transition: background-color 0.2s ease-in-out;
-
-//   &:hover,
-//   &:focus {
-//     background-color: #ffffff;
-//     color: #000000;
-//   }
-// `
 export function RainbowkitPage() {
   const { openConnectModal } = useConnectModal()
   const { openAccountModal } = useAccountModal()
