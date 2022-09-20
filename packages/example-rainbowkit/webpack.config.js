af82dd179f3a27f1e5a819cce26c84a2f6d41ef7
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2022-09-20 11:40:46 +0200

    Fix css-loader

--- packages/example-rainbowkit/webpack.config.js
+++ packages/example-rainbowkit/webpack.config.js
@@ -44,6 +44,10 @@ module.exports = {
         test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf|ico)$/,
         use: ['file-loader'],
       },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   resolve: {
