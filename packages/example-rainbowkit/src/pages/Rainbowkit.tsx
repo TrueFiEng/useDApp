1be81c541a62611ec867c1815483ea97b54a2b52
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2022-09-22 10:29:17 +0200

    Style changes

--- packages/example-rainbowkit/src/pages/Rainbowkit.tsx
+++ packages/example-rainbowkit/src/pages/Rainbowkit.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Container, MainContent } from '../components/base/base'
 import { ConnectButton, useAccountModal, useChainModal, useConnectModal } from '@usedapp/rainbowkit'
-import { Button } from '../components/base/Button';
+import { Button } from '../components/base/Button'
+import { AccountButton } from '../components/account/AccountButton'
 
 export function RainbowkitPage() {
   const { openConnectModal } = useConnectModal()
@@ -10,29 +11,36 @@ export function RainbowkitPage() {
 
   return (
     <MainContent>
-      <Container>
-        <br />
-        <ConnectButton />
-        <br />
-        {openConnectModal && (
-          <Button onClick={openConnectModal} type="button">
-            Open Connect Modal
-          </Button>
-        )}
-        <br />
-        {openAccountModal && (
-          <Button onClick={openAccountModal} type="button">
-            Open Account Modal
-          </Button>
-        )}
-        <br />
-        {openChainModal && (
-          <Button onClick={openChainModal} type="button">
-            Open Chain Modal
-          </Button>
-        )}
-        <br />
-      </Container>
+      <div
+        style={{
+          marginLeft: '300px',
+        }}
+      >
+        <Container>
+          <AccountButton />
+          <br />
+          <ConnectButton />
+          <br />
+          {openConnectModal && (
+            <Button onClick={openConnectModal} type="button">
+              Open Connect Modal
+            </Button>
+          )}
+          <br />
+          {openAccountModal && (
+            <Button onClick={openAccountModal} type="button">
+              Open Account Modal
+            </Button>
+          )}
+          <br />
+          {openChainModal && (
+            <Button onClick={openChainModal} type="button">
+              Open Chain Modal
+            </Button>
+          )}
+          <br />
+        </Container>
+      </div>
     </MainContent>
   )
 }
