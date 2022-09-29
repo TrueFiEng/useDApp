9270f332717daa96c39ea37224037708f9432755
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2022-09-29 10:51:17 +0200

    Add testnets and import

--- packages/core/src/model/chain/optimism.ts
+++ packages/core/src/model/chain/optimism.ts
@@ -10,6 +10,11 @@ export const OptimismKovan: Chain = {
   isLocalChain: false,
   multicallAddress: '0xE71bf4622578c7d1526A88CD3060f03030E99a04',
   rpcUrl: 'https://kovan.optimism.io',
+  nativeCurrency: {
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
   blockExplorerUrl: testnetExplorerUrl,
   getExplorerAddressLink: getAddressLink(testnetExplorerUrl),
   getExplorerTransactionLink: getTransactionLink(testnetExplorerUrl),
@@ -24,6 +29,11 @@ export const OptimismGoerli: Chain = {
   isLocalChain: false,
   multicallAddress: '0xC8315CC7DCDF57476a8a1D184505845d52711024',
   rpcUrl: 'https://goerli.optimism.io',
+  nativeCurrency: {
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
   blockExplorerUrl: testnetGoerliExplorerUrl,
   getExplorerAddressLink: getAddressLink(testnetGoerliExplorerUrl),
   getExplorerTransactionLink: getTransactionLink(testnetGoerliExplorerUrl),
@@ -50,5 +60,6 @@ export const Optimism: Chain = {
 
 export default {
   OptimismKovan,
+  OptimismGoerli,
   Optimism,
 }
