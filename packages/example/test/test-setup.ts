5b920ee95da738721fcbdeeca33b6c0295d68457
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2022-10-06 13:02:22 +0200

    Add timeout

--- packages/example/test/test-setup.ts
+++ packages/example/test/test-setup.ts
@@ -1,9 +1,12 @@
 import chai from 'chai'
 import { solidity } from 'ethereum-waffle'
 import chaiAsPromised from 'chai-as-promised'
+import waitForExpect from 'wait-for-expect'
 
 chai.use(solidity)
 chai.use(chaiAsPromised)
+
+waitForExpect.defaults.timeout = 20000
 
 let jsdomCleanup: any
 before(() => {
