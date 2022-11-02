3737357c3d57c0d133977407ba2c244e6a6cb0bc
Jakub Sieczczyński <j.sieczczynski@gmail.com>
2022-11-02 14:36:17 +0100

    Lint

--- packages/siwe/src/provider.tsx
+++ packages/siwe/src/provider.tsx
@@ -127,15 +127,10 @@ export const SiweProvider = ({ children, backendUrl, api }: SiweProviderProps) =
       if (!account || !chainId || !library) {
         return
       }
-<<<<<<< HEAD
-      const signer = library.getSigner()
-      const nonce = await getNonceHandler()
-=======
       const signer = 'getSigner' in library ? library.getSigner() : undefined
       if (!signer) return
-      const nonce = await getNonceRequestHandler()
->>>>>>> origin/master
-
+      
+      const nonce = await getNonceHandler()
       if (!nonce) return
 
       const message = new SiweMessage({
