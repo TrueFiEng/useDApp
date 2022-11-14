f0fc0c1e6cdfc0665d24b897b7e49deefff7a803
Jakub Sieczczyński <75097304+jakvbs@users.noreply.github.com>
2022-11-14 11:26:02 +0100

    🥦 Update e2e tests to new Gnosis Safe app version (#1025)
    * Update e2e tests to new Gnosis Safe app version
    
    * Add timeout
    
    * Add waitForTransaction
    
    * Restore
    
    * Selectors refactor
    
    * Test push 1
    
    * Test push 2
    
    * Refactor
    
    * Add second attempt to submit
    
    * Add comments

--- packages/playwright/src/xpath.ts
+++ packages/playwright/src/xpath.ts
@@ -1,13 +1,13 @@
 export class XPath {
-  static text(element: keyof HTMLElementTagNameMap, text: string, occurrence = 1) {
+  static text(element: keyof HTMLElementTagNameMap | '*', text: string, occurrence = 1) {
     return `(//${element}[contains(text(), "${text}")])[${occurrence}]`
   }
 
-  static id(element: keyof HTMLElementTagNameMap, text: string) {
+  static id(element: keyof HTMLElementTagNameMap | '*', text: string) {
     return `//${element}[contains(@id, "${text}")]`
   }
 
-  static class(element: keyof HTMLElementTagNameMap, text: string) {
+  static class(element: keyof HTMLElementTagNameMap | '*', text: string) {
     return `//${element}[contains(@class, ${text})]`
   }
 
