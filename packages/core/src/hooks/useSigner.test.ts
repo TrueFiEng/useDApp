d57fc9fd9e849d6bfe8c9605a323910cb06467c6
pawelpolak2 <pawel.ulic@gmail.com>
2022-12-14 17:20:11 +0100

    factor 2

--- packages/core/src/hooks/useSigner.test.ts
+++ packages/core/src/hooks/useSigner.test.ts
@@ -1,9 +1,8 @@
-import { JsonRpcProvider, JsonRpcSigner } from '@ethersproject/providers'
 import { expect } from 'chai'
 import { BigNumber, Wallet } from 'ethers'
 import { useEffect } from 'react'
 import { Config } from '../constants'
-import { renderDAppHook, setupTestingConfig, sleep, TestingNetwork } from '../testing'
+import { renderDAppHook, setupTestingConfig, sleep } from '../testing'
 import { useEthers } from './useEthers'
 import { useSigner } from './useSigner'
 
