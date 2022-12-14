993af36067a5dc117022533bc0292d13d14e3832
pawelpolak2 <pawel.ulic@gmail.com>
2022-12-14 17:10:12 +0100

    format

--- packages/core/src/hooks/useSigner.tsx
+++ packages/core/src/hooks/useSigner.tsx
@@ -1,15 +1,15 @@
-import { useEthers } from "./useEthers";
-import { useState, useEffect } from "react";
-import { JsonRpcProvider, JsonRpcSigner } from "@ethersproject/providers";
+import { useEthers } from './useEthers'
+import { useState, useEffect } from 'react'
+import { JsonRpcProvider, JsonRpcSigner } from '@ethersproject/providers'
 
-export function useSigner() : JsonRpcSigner | undefined{
-    const {library, account} = useEthers();
-    const [signer, setSigner] = useState<JsonRpcSigner | undefined>();
+export function useSigner(): JsonRpcSigner | undefined {
+  const { library, account } = useEthers()
+  const [signer, setSigner] = useState<JsonRpcSigner | undefined>()
 
-    useEffect(() => {
-        if(account) setSigner((library as JsonRpcProvider).getSigner());
-        else setSigner(undefined);
-    }, [account, library]);
+  useEffect(() => {
+    if (account) setSigner((library as JsonRpcProvider).getSigner())
+    else setSigner(undefined)
+  }, [account, library])
 
-    return signer;
+  return signer
 }
