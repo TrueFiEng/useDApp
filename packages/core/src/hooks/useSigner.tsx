d0a3c0aae6230f4feaa83da443de447278cbb172
pawelpolak2 <pawel.ulic@gmail.com>
2022-12-15 15:40:49 +0100

    refactor

--- packages/core/src/hooks/useSigner.tsx
+++ packages/core/src/hooks/useSigner.tsx
@@ -2,12 +2,16 @@ import { useEthers } from './useEthers'
 import { useState, useEffect } from 'react'
 import { JsonRpcProvider, JsonRpcSigner } from '@ethersproject/providers'
 
+/**
+ * Returns a signer.
+ * @returns a JsonRpcSigner if one is available in the provider. 'undefined' otherwise.
+ */
 export function useSigner(): JsonRpcSigner | undefined {
   const { library, account } = useEthers()
   const [signer, setSigner] = useState<JsonRpcSigner | undefined>()
 
   useEffect(() => {
-    if (account) setSigner((library as JsonRpcProvider).getSigner())
+    if (library !== undefined && 'getSigner' in library) setSigner((library as JsonRpcProvider).getSigner())
     else setSigner(undefined)
   }, [account, library])
 
