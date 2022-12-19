42a79ce1ea2c72ad702cdd88c7bcd8a97fe5cdc8
pawelpolak2 <pawel.ulic@gmail.com>
2022-12-19 18:14:18 +0100

    Optimism goerli

--- packages/example/src/components/Transactions/Forms.tsx
+++ packages/example/src/components/Transactions/Forms.tsx
@@ -4,6 +4,7 @@ import {
   Goerli,
   Mainnet,
   Optimism,
+  OptimismGoerli,
   Rinkeby,
   Ropsten,
   useContractFunction,
@@ -18,12 +19,15 @@ import { Weth10 } from '../../../gen/types/Weth10'
 import WethAbi from '../../abi/Weth10.json'
 
 const wethInterface = new utils.Interface(WethAbi.abi)
+
+// using weth9 addresses due to bigger usage
 const wethContractAddresses = {
   [Mainnet.chainId]: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
   [Rinkeby.chainId]: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
   [Goerli.chainId]: '0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6',
   [Ropsten.chainId]: '0xc778417E063141139Fce010982780140Aa0cD5Ab',
   [Optimism.chainId]: '0x4200000000000000000000000000000000000006',
+  [OptimismGoerli.chainId]: '0x09bADef78f92F20fd5f7a402dbb1d25d4901aAb2'
 }
 
 export const DepositEth = () => {
