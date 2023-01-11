2adbea17d15a5b586ee29d965fec87a9eca9fb8e
Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>
2023-01-11 10:45:28 +0100

    🫐 Simplify useChainId hook (#1059)

--- packages/core/src/hooks/useChainId.ts
+++ packages/core/src/hooks/useChainId.ts
@@ -1,7 +1,6 @@
 import { QueryParams } from '../constants/type/QueryParams'
 import { useConnector } from '../providers/network/connectors'
 import { useConfig } from '../hooks'
-import { useEffect, useState } from 'react'
 
 export interface UseChainIdOptions {
   queryParams?: QueryParams
@@ -12,21 +11,8 @@ export interface UseChainIdOptions {
  * @internal Intended for internal use - use it on your own risk
  */
 export function useChainId(opts: UseChainIdOptions = {}) {
-  const { connector } = useConnector()
-
+  const { chainId } = useConnector()
   const { readOnlyChainId } = useConfig()
-  const [chainId, setChainId] = useState<number | undefined>()
 
-  useEffect(() => {
-    setChainId(connector?.chainId)
-
-    if (!connector) {
-      return
-    }
-
-    return connector.updated.on(({ chainId }) => {
-      setChainId(chainId)
-    })
-  }, [connector])
   return opts?.queryParams?.chainId ?? chainId ?? readOnlyChainId
 }
