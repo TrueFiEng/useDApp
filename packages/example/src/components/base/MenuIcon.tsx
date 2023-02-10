feb2f35f185bb018eae65c0a362adc1f27600ec9
Paweł Ulicki <67280173+pawelpolak2@users.noreply.github.com>
2023-02-10 12:21:12 +0100

    👕 Add basic functionality to show/hide the sidebar in example app (#1084)
    * feat(NavBar): adds basic functionality to show/hide the sidebar
    
    * fix(lint): run yarn lint:fix
    
    * Extend the changes to the example-next
    
    * add .pnpm-debug.log to gitignore
    
    ---------
    
    Co-authored-by: Jay Welsh <thealchemicalopus@gmail.com>

---
+++ packages/example/src/components/base/MenuIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const MenuIcon = () => (
+  <svg viewBox="0 0 100 80" width="40" height="40">
+    <rect fill="#232429" width="100" height="20"></rect>
+    <rect fill="#232429" y="30" width="100" height="20"></rect>
+    <rect fill="#232429" y="60" width="100" height="20"></rect>
+  </svg>
+)
--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -10,7 +10,10 @@ export {
   Handshaking,
   Page,
   SidebarContainer,
+  MenuIconContainer,
+  SidebarMenuIconContainer,
 } from './components/base/base'
+export { MenuIcon } from './components/base/MenuIcon'
 export { ENSExample } from './components/ENS/ENSExample'
 export { GlobalStyle } from './global/GlobalStyle'
 export * from './global/styles'
