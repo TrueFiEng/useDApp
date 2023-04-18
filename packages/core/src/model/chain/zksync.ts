958cb84806d39a6ba24cbd814bbdade4051dba48
Paweł Ulicki <67280173+pawelpolak2@users.noreply.github.com>
2023-04-18 12:32:08 +0200

    👃 Add ZkSync chain (#1105)
    * add zkSync era
    
    * Add ZkSync
    
    ---------
    
    Co-authored-by: Krunal Amin <kishanamin97@gmail.com>
    Co-authored-by: Vladyslav Yatsenko <52505649+yivlad@users.noreply.github.com>

--- packages/core/src/model/chain/zksync.ts
+++ packages/core/src/model/chain/zksync.ts
@@ -1,7 +1,24 @@
 import { Chain } from '../../constants'
 import { getAddressLink, getTransactionLink } from '../../helpers/chainExplorerLink'
 
-const zksyncExplorerUrl = 'https://zksync2-testnet.zkscan.io'
+const zksyncExplorerUrl = 'https://zksync2-mainnet.zkscan.io/'
+
+export const ZkSync: Chain = {
+  chainId: 324,
+  chainName: 'zkSync Era',
+  isTestChain: false,
+  isLocalChain: false,
+  multicallAddress: '0x1De2452e12b982Bc31500FcfD1AFf8FfdAd93CBa',
+  multicall2Address: '0x7541c21A0D384B0f734d4C091fd40125F7Ad9AE1',
+  rpcUrl: 'https://mainnet.era.zksync.io',
+  nativeCurrency: {
+    name: 'ETH',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  getExplorerAddressLink: getAddressLink(zksyncExplorerUrl),
+  getExplorerTransactionLink: getTransactionLink(zksyncExplorerUrl),
+}
 
 export const ZkSyncTestnet: Chain = {
   chainId: 280,
