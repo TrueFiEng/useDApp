c112082f57bfd5c7c5ac0b0d22ada8bf083eec1e
Arthur Abeilice <afa7789@gmail.com>
2023-08-01 09:40:07 -0300

    🪥 Adding base (#1131)

---
+++ .changeset/add_Base.md
@@ -0,0 +1,6 @@
+@@ -0,0 +1,5 @@
+---
+"@usedapp/core": patch
+---
+
+⛓ Add Base Mainnet to chain ID
--- packages/core/src/model/chain/base.ts
+++ packages/core/src/model/chain/base.ts
@@ -20,4 +20,26 @@ export const BaseGoerli: Chain = {
   getExplorerTransactionLink: getTransactionLink(baseGoerliUrl),
 }
 
-export default { BaseGoerli }
+const baseUrl = 'https://basescan.org'
+
+export const Base: Chain = {
+  chainId: 8453,
+  chainName: 'Base',
+  isTestChain: false,
+  isLocalChain: false,
+  multicallAddress: '0x38641b7a50CDcfde75a7A83eB7c02581F3279362',
+  rpcUrl: 'https://base.org',
+  nativeCurrency: {
+    name: 'Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  blockExplorerUrl: baseUrl,
+  getExplorerAddressLink: getAddressLink(baseUrl),
+  getExplorerTransactionLink: getTransactionLink(baseUrl),
+}
+
+export default {
+  BaseGoerli,
+  Base,
+}
