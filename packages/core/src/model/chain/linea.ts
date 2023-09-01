4e74583ccc6a1d907af3f3928cdf7613d854ae9c
Arthur Abeilice <afa7789@gmail.com>
2023-09-01 14:48:25 -0300

    📟 Adding chains (#1138)
    * Adding chains
    
    * lint
    
    ---------
    
    Co-authored-by: pawelpolak2 <pawel.ulic@gmail.com>

--- packages/core/src/model/chain/linea.ts
+++ packages/core/src/model/chain/linea.ts
@@ -1,7 +1,7 @@
 import { Chain } from '../../constants'
 import { getAddressLink, getTransactionLink } from '../../helpers/chainExplorerLink'
 
-const lineaExplorerUrl = 'https://explorer.goerli.linea.build'
+const lineaExplorerUrlTestnet = 'https://explorer.goerli.linea.build'
 
 export const LineaTestnet: Chain = {
   chainId: 59140,
@@ -10,6 +10,25 @@ export const LineaTestnet: Chain = {
   isLocalChain: false,
   multicallAddress: '0x9901f3053527a58B8C210B144f53CbeA7b6E23Fb',
   rpcUrl: 'https://rpc.goerli.linea.build',
+  nativeCurrency: {
+    name: 'Linea Ether',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  blockExplorerUrl: lineaExplorerUrlTestnet,
+  getExplorerAddressLink: getAddressLink(lineaExplorerUrlTestnet),
+  getExplorerTransactionLink: getTransactionLink(lineaExplorerUrlTestnet),
+}
+
+const lineaExplorerUrl = 'https://lineascan.build/'
+
+export const Linea: Chain = {
+  chainId: 59144,
+  chainName: 'Linea',
+  isTestChain: false,
+  isLocalChain: false,
+  multicallAddress: '0x5D155A04De5bB501f022E44AEd7FF23A6Ff2C1F1',
+  rpcUrl: 'https://rpc.linea.build',
   nativeCurrency: {
     name: 'Linea Ether',
     symbol: 'ETH',
