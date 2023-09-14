0b953e8ee316f7b0d5fd3a88007dd525788b25c4
Arthur Abeilice <afa7789@gmail.com>
2023-09-14 17:11:48 -0300

    🐳 Add kroma network (#1142)
    * Adding chain kroma
    
    * adding multicall address
    
    * Update chainId.ts
    
    * Update .changeset/adding_kroma.md
    
    * Fix lint
    
    ---------
    
    Co-authored-by: Łukasz Stankiewicz <stankiewicz1998@gmail.com>

---
+++ .changeset/adding_kroma.md
@@ -0,0 +1,11 @@
+---
+"@usedapp/core": patch
+---
+
+Adding the network kroma mainnet:
+
+https://blockscout.kroma.network/
+
+https://kroma.network/
+
+it does not have verified contracts yet in the explorer, adding multicall address later.
--- packages/core/src/model/chain/kroma.ts
+++ packages/core/src/model/chain/kroma.ts
@@ -19,3 +19,22 @@ export const KromaSepoliaTestnet: Chain = {
   getExplorerAddressLink: getAddressLink(KromaSepoliaExplorerUrl),
   getExplorerTransactionLink: getTransactionLink(KromaSepoliaExplorerUrl),
 }
+
+const KromaExplorerUrl = 'https://blockscout.kroma.network/'
+
+export const KromaMainnet: Chain = {
+  chainId: 255,
+  chainName: 'Kroma',
+  isTestChain: false,
+  isLocalChain: false,
+  multicallAddress: '0xE3c886498ac54433F2B0E6842FAE421006067751',
+  rpcUrl: 'https://api.kroma.network/',
+  nativeCurrency: {
+    name: 'ETH',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  blockExplorerUrl: KromaExplorerUrl,
+  getExplorerAddressLink: getAddressLink(KromaExplorerUrl),
+  getExplorerTransactionLink: getTransactionLink(KromaExplorerUrl),
+}
