53c335bc39bf31b3e441a25ef184a11b48e7f0b4
Tyler Loewen <tyler@trilez.com>
2024-05-09 16:20:03 -0700

    Disable buggy optimization

--- packages/core/src/hooks/useResolvedPromise.ts
+++ packages/core/src/hooks/useResolvedPromise.ts
@@ -1,11 +1,9 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect } from 'react'
 import { Falsy } from '../model'
 import { deepEqual } from '../helpers'
 
 export function useResolvedPromise<T>(promise: Promise<T> | T | Falsy): T | Falsy {
   const [resolvedValue, setResolvedValue] = useState<T | Falsy>(promise instanceof Promise ? undefined : promise)
-
-  const lastPromiseRef = useRef<Promise<T> | T | Falsy>(undefined)
 
   useEffect(() => {
     let active = true // Flag to prevent setting state after component unmounts
@@ -17,18 +15,14 @@ export function useResolvedPromise<T>(promise: Promise<T> | T | Falsy): T | Fals
         return
       }
 
-      if (promise !== lastPromiseRef.current) {
-        lastPromiseRef.current = promise
+      // If the input is not a promise, it directly sets the resolved value
+      const value: T | Falsy = await promise
 
-        // If the input is not a promise, it directly sets the resolved value
-        const value: T | Falsy = await promise
-
-        // Check if the component is still mounted before setting the state
-        if (active) {
-          if (!deepEqual(resolvedValue, value)) {
-            // Calling setResolvedValue will trigger a rerender, so we only call it if the value has changed
-            setResolvedValue(value)
-          }
+      // Check if the component is still mounted before setting the state
+      if (active) {
+        if (!deepEqual(resolvedValue, value)) {
+          // Calling setResolvedValue will trigger a rerender, so we only call it if the value has changed
+          setResolvedValue(value)
         }
       }
     }
